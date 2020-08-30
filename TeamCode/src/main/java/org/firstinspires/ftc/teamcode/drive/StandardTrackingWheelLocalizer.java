1c459ae423ace639b69995ee533150855ef48096
Henopied <henopied@users.noreply.github.com>
2020-08-30 17:14:20 -0500

    Clarify affected encoders

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -67,8 +67,9 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     @NonNull
     @Override
     public List<Double> getWheelVelocities() {
-        // TODO: If your encoder velocity can exceed 32767 counts / second, change Encoder.getRawVelocity() to
-        // Encoder.getCorrectedVelocity() to enable a compensation method
+        // TODO: If your encoder velocity can exceed 32767 counts / second (such as the REV Through Bore and other
+        //  competing magnetic encoders), change Encoder.getRawVelocity() to Encoder.getCorrectedVelocity() to enable a
+        //  compensation method
 
         return Arrays.asList(
                 encoderTicksToInches(leftEncoder.getRawVelocity()),
