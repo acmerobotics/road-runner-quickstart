00ccac20a6a59a43e09e60560683132a615615e6
Noah Bresler <noahbresler2002@gmail.com>
2021-03-12 00:30:48 -0600

    Move TrajectorySequence list to an internal value

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/trajectorysequence/TrajectorySequence.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/trajectorysequence/TrajectorySequence.java
@@ -4,30 +4,41 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 
 import org.firstinspires.ftc.teamcode.trajectorysequence.sequencesegment.SequenceSegment;
 
-import java.util.LinkedList;
+import java.util.Collections;
+import java.util.List;
 
-public class TrajectorySequence extends LinkedList<SequenceSegment> {
+public class TrajectorySequence {
+    private final List<SequenceSegment> sequenceList;
+
+    public TrajectorySequence(List<SequenceSegment> sequenceList) {
+        if (sequenceList.size() == 0) throw new EmptySequenceException();
+
+        this.sequenceList = Collections.unmodifiableList(sequenceList);
+    }
+
     public Pose2d start() {
-        if (!this.isEmpty())
-            return this.get(0).getStartPose();
-        else
-            return new Pose2d();
+        return sequenceList.get(0).getStartPose();
     }
 
     public Pose2d end() {
-        if (!this.isEmpty())
-            return this.get(this.size() - 1).getEndPose();
-        else
-            return new Pose2d();
+        return sequenceList.get(sequenceList.size() - 1).getEndPose();
     }
 
     public double duration() {
         double total = 0.0;
 
-        for (SequenceSegment segment : this) {
+        for (SequenceSegment segment : sequenceList) {
             total += segment.getDuration();
         }
 
         return total;
+    }
+
+    public SequenceSegment get(int i) {
+        return sequenceList.get(i);
+    }
+
+    public int size() {
+        return sequenceList.size();
     }
 }
