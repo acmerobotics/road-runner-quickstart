d55bef8c366a6fb39e5aa965e28eb8a58e681705
Noah Bresler <noahbresler2002@gmail.com>
2021-04-21 17:07:22 -0500

    Add note on markers being stored in the `Trajectory` itself

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/trajectorysequence/sequencesegment/TrajectorySegment.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/trajectorysequence/sequencesegment/TrajectorySegment.java
@@ -8,6 +8,8 @@ public final class TrajectorySegment extends SequenceSegment {
     private final Trajectory trajectory;
 
     public TrajectorySegment(Trajectory trajectory) {
+        // Note: Markers are already stored in the `Trajectory` itself.
+        // This class should not hold any markers
         super(trajectory.duration(), trajectory.start(), trajectory.end(), Collections.emptyList());
         this.trajectory = trajectory;
     }
