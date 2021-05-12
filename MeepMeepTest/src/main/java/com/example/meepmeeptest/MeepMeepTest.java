9d5e2d9dc9454d18f1203c1fda9149037ced6c9f
alanstvn <61133920+alanstvn@users.noreply.github.com>
2021-05-12 18:42:09 -0500

    Update MeepMeepTest.java
    add red blue 2 path with waits

--- MeepMeepTest/src/main/java/com/example/meepmeeptest/MeepMeepTest.java
+++ MeepMeepTest/src/main/java/com/example/meepmeeptest/MeepMeepTest.java
@@ -1,6 +1,8 @@
 package com.example.meepmeeptest;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.constraints.TranslationalVelocityConstraint;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
 
@@ -16,17 +18,35 @@ public class MeepMeepTest {
                 // Background opacity from 0-1
                 .setBackgroundAlpha(1f)
                 // Set constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(99, 30, Math.toRadians(37.74), Math.toRadians(60), 37)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
+                        drive.trajectorySequenceBuilder(new Pose2d(-60, 48, 0))
+                                .lineTo(new Vector2d(-24,48))
+                                .addTemporalMarker(15.0,() -> {
+                                    // do stuff
+                                })
+                                .splineTo(new Vector2d(36, 19), Math.toRadians(0))
+                                .addDisplacementMarker(() -> {
+                                    // drop wobble goal
+                                    //return Unit.INSTANCE;
+                                })
+                                .waitSeconds(1.5)
+                                .lineToConstantHeading(new Vector2d(-18, 19))
+                                // slowly approach wobble goal
+                                .lineTo(new Vector2d(-27, 19),new TranslationalVelocityConstraint(5))
+                                // grab wobble goal
+                                .addDisplacementMarker(() -> {
+                                    // grab wobble goal
+                                    //return Unit.INSTANCE;
+                                })
+                                .waitSeconds(2.0)
+                                .lineToSplineHeading(new Pose2d(20, 19, Math.toRadians(-135)))
+                                .addDisplacementMarker(() -> {
+                                    // drop wobble goal
+                                    //return Unit.INSTANCE;
+                                })
+                                .waitSeconds(1.0)
+                                .forward(6)
                                 .build()
                 )
                 .start();
