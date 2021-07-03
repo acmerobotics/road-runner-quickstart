9e3e7b6af854ad52b05beeca430552482e634752
SreyashDasSarma <69434791+SreyashDasSarma@users.noreply.github.com>
2021-07-02 20:21:56 -0700

    Update SampleSubsystemAuton.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleAuton/SampleSubsystemAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleAuton/SampleSubsystemAuton.java
@@ -13,17 +13,13 @@ public class SampleSubsystemAuton extends LinearOpMode {
         HardwareFile drive = new HardwareFile(hardwareMap);
 
         waitForStart();
-
         if (isStopRequested()) return;
-        drive.shooter(-1);
+        drive.grab();
+        sleep(100);
+        drive.wobbleArmDown();
         sleep(1000);
-        for(int i=0;i<3;++i){
-           drive.slapper.setPosition(0.35);
-            sleep(500);
-            drive.slapper.setPosition(0.65);
-            sleep(500);
-        }
-        drive.shooter(0);
+        drive.release();
+        sleep(100);
     }
 
 }
