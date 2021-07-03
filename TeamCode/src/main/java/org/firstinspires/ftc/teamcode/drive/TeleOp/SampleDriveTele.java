3f48142927c526106e269a1099eff3c0d05b4b78
SreyashDasSarma <69434791+SreyashDasSarma@users.noreply.github.com>
2021-07-02 20:31:09 -0700

    Update SampleDriveTele.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TeleOp/SampleDriveTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TeleOp/SampleDriveTele.java
@@ -1,30 +1,17 @@
-package org.firstinspires.ftc.teamcode.drive.TeleOp;
+package org.firstinspires.ftc.teamcode.drive.SampleAutonPaths;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import org.firstinspires.ftc.teamcode.drive.Hardware.Robot;
-import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotorController;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.Blinker;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.Gyroscope;
-import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+//import org.firstinspires.ftc.teamcode.drive.Hardware.Robot;
 @TeleOp(name="TwoDrivers", group="Linear Opmode")
-public class MainTele extends LinearOpMode implements Runnable{
-    Robot robot;
+public class SampleDriveTele extends LinearOpMode implements Runnable{
+    HardwareFile robot;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        robot = new Robot(hardwareMap);
+        robot = new HardwareFile(hardwareMap);
         waitForStart();
         while (opModeIsActive()) {
             run();
