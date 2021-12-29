2cc697600994ee5cd3a175f616d65b03543a49e7
turtlelazy <ishraqmahid@gmail.com>
2021-12-29 14:31:12 -0500

    dttest opemode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opMode/descr.md
@@ -0,0 +1,3 @@
+# What's here?
+
+Here lies auton, teleOp, and prototyping op mode for the TEAM
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opMode/protoType/DrivetrainTest.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.opMode.protoType;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+@Config
+@TeleOp(name="DrivetrainTest",group="TeleOp")
+public class DrivetrainTest extends LinearOpMode{
+
+     @Override
+        public void runOpMode() throws InterruptedException {
+            Pose2d controls = new Pose2d(
+                    -gamepad1.left_stick_y,
+                    -gamepad1.left_stick_x,
+                    -gamepad1.right_stick_x
+            );
+
+            SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+            drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+            while(!opModeIsActive() && !isStopRequested()){
+                telemetry.addData("Status", "Waiting in init");
+                telemetry.update();
+            }
+            if(!drive.isBusy()) drive.setWeightedDrivePower(controls);
+
+        }
+}
+
+
