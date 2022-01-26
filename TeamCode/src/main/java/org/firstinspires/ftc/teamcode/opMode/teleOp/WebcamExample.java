fb7e01461fcd54430d2645c9097e092dd0ed2a43
Zawad Dewan <zawaddewan@gmail.com>
2022-01-26 08:17:15 -0500

    made the kellen cv class its own class in hardware

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opMode/teleOp/WebcamExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opMode/teleOp/WebcamExample.java
@@ -26,17 +26,11 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
-import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
-import org.openftc.easyopencv.OpenCvPipeline;
 import org.openftc.easyopencv.OpenCvWebcam;
+import org.firstinspires.ftc.teamcode.hardware.kellen;
 
 
 @TeleOp
@@ -188,121 +182,4 @@ public class WebcamExample extends LinearOpMode
      * if you're doing something weird where you do need it synchronized with your OpMode thread,
      * then you will need to account for that accordingly.
      */
-    class kellen extends OpenCvPipeline
-    {
-        public kellen() {
-
-        }
-        boolean viewportPaused;
-
-        private Mat workingMatrix = new Mat();
-
-        private double regionValue;
-
-        private Rect RO1;
-        private Rect RO2;
-        private Rect RO3;
-
-        @Override
-        public final Mat processFrame(Mat input){
-            //cvtColor converts a feed from a certain color format to another color format; in this case, we are converting from RGB to HSV
-            Imgproc.cvtColor(input, workingMatrix, Imgproc.COLOR_RGB2HSV);
-
-        /*lowHSV and highHSV are our thresholds
-        IMPORTANT NOTE: openCV defines HSV parameters as such (Hue, Saturation, Value) where Hue is Range 0-179,
-        Saturation is in range 0-255 and Value is in range 0-255 (all INCLUSIVE).
-        NORMALLY, HSV is defined like so: Hue in range 0-360, Saturation in range 0.0-1.0 and Value in range 0.0-1.0.
-        All of this is also technically BGR to HSV, so take the absolute value of your Hue minus 180 to get the right number
-        */
-            Scalar lowHSV = new Scalar(40, 50, 50);
-            Scalar highHSV = new Scalar(75, 255, 255);
-            //This creates our mask, and filters out all colors except for whats within our defined bound
-            /* IGNORE ALL OF THIS FOR NOW, but essentially we'll use this to tell where our capstone is by counting pixels
-
-            creates the submat that we want to work with
-            Mat region = workingMatrix.submat(ROI);
-            //this counts the number of white pixels and divides it by the area of our ROI to figure out the percentage.
-            regionValue = Core.sumElems(region).val[0] / ROI.area()/255;
-            //you need to release the channel that we worked with or smthn smhtn; in this case we have to release region for some reason
-            region.release();
-            //line color
-            Scalar lines = new Scalar(25,255,255);
-            //Create the rectangle so that when testing we can see the ROI that we are working with
-            Imgproc.rectangle(workingMatrix,ROI,lines);*/
-            RO1 = new Rect(
-                    new Point(
-                            input.cols()/8,
-                            input.rows()/4),
-                    new Point(
-                            input.cols()*(3f/8f),
-                            input.rows()*(3f/4f))
-            );
-            RO2 = new Rect(
-                    new Point(
-                            input.cols()*(3f/8f),
-                            input.rows()/4),
-                    new Point(
-                            input.cols()*(5f/8f),
-                            input.rows()*(3f/4f))
-            );
-            RO3 = new Rect(
-                    new Point(
-                            input.cols()*(5f/8f),
-                            input.rows()/4),
-                    new Point(
-                            input.cols()*(7f/8f),
-                            input.rows()*(3f/4f))
-            );
-            Imgproc.rectangle(workingMatrix, RO1, new Scalar(60, 255, 255), 10);
-            Imgproc.rectangle(workingMatrix, RO2, new Scalar(60, 255, 255), 10);
-            Imgproc.rectangle(workingMatrix, RO3, new Scalar(60, 255, 255), 10);
-            Core.inRange(workingMatrix, lowHSV, highHSV, workingMatrix);
-
-            //Submats for boxes, these are the regions that'll detect the color
-
-            Mat box1 = workingMatrix.submat(RO1);
-            Mat box2 = workingMatrix.submat(RO2);
-            Mat box3 = workingMatrix.submat(RO3);
-            //How much in each region is white aka the color we filtered
-            double b1p = Core.sumElems(box1).val[0] / RO1.area()/255;
-            double b2p = Core.sumElems(box2).val[0] / RO2.area()/255;
-            double b3p = Core.sumElems(box3).val[0] / RO3.area()/255;
-            //Compare amount of color in each region
-            if(b1p > b2p && b1p > b3p) {
-                Imgproc.rectangle(workingMatrix, RO1, new Scalar(60, 255, 255), 10);
-            }else if(b2p > b1p && b2p > b3p) {
-                Imgproc.rectangle(workingMatrix, RO2, new Scalar(60, 255, 255), 10);
-            }else if(b3p > b2p & b3p > b1p) {
-                Imgproc.rectangle(workingMatrix, RO3, new Scalar(60, 255, 255), 10);
-            }
-            //return the frame
-            return workingMatrix;
-        }
-        @Override
-        public void onViewportTapped()
-        {
-            /*
-             * The viewport (if one was specified in the constructor) can also be dynamically "paused"
-             * and "resumed". The primary use case of this is to reduce CPU, memory, and power load
-             * when you need your vision pipeline running, but do not require a live preview on the
-             * robot controller screen. For instance, this could be useful if you wish to see the live
-             * camera preview as you are initializing your robot, but you no longer require the live
-             * preview after you have finished your initialization process; pausing the viewport does
-             * not stop running your pipeline.
-             *
-             * Here we demonstrate dynamically pausing/resuming the viewport when the user taps it
-             */
-
-            viewportPaused = !viewportPaused;
-
-            if(viewportPaused)
-            {
-                webcam.pauseViewport();
-            }
-            else
-            {
-                webcam.resumeViewport();
-            }
-        }
-    }
 }
