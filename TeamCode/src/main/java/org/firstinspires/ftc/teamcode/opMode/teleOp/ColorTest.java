a10e0f8f36941ab05f4ee20013efa13fcd58dea4
Zawad Dewan <zawaddewan@gmail.com>
2022-01-28 08:28:33 -0500

    colooooooors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opMode/teleOp/ColorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opMode/teleOp/ColorTest.java
@@ -4,27 +4,20 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.hardware.FreightSensor;
+
 @TeleOp
 public class ColorTest extends LinearOpMode {
-    // Define a variable for our color sensor
-    ColorSensor color;
-    public boolean isBlock() {
-        return ((color.red() + color.green()) / 2 > color.blue() && (color.red()+color.green()) / 2 >= 100);
-    }
+    FreightSensor color = new FreightSensor();
     @Override
     public void runOpMode() {
-        // Get the color sensor from hardwareMap
-        color = hardwareMap.get(ColorSensor.class, "color");
 
         // Wait for the Play button to be pressed
         waitForStart();
 
         // While the Op Mode is running, update the telemetry values.
-        while (opModeIsActive()) {
-            telemetry.addData("Red", color.red());
-            telemetry.addData("Green", color.green());
-            telemetry.addData("Blue", color.blue());
-            telemetry.addData("blocc", isBlock());
+        while (opModeIsActive()) { ;
+            telemetry.addData("blocc", color.hasFreight());
             telemetry.update();
         }
     }
