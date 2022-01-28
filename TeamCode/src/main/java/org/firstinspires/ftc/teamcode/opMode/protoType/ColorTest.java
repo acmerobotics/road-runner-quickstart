c2e5d943af6bd622f222d298f1263ec261a94ab9
Zawad Dewan <zawaddewan@gmail.com>
2022-01-28 18:25:14 -0500

    rumburuuuuuuu

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opMode/protoType/ColorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opMode/protoType/ColorTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.opMode.teleOp;
+package org.firstinspires.ftc.teamcode.opMode.protoType;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
@@ -11,16 +11,27 @@ public class ColorTest extends LinearOpMode {
     FreightSensor color = new FreightSensor();
     @Override
     public void runOpMode() {
+        boolean hadFreight = false;
         color.init(hardwareMap);
         // Wait for the Play button to be pressed
         waitForStart();
 
         // While the Op Mode is running, update the telemetry values.
         while (opModeIsActive()) { ;
+
             telemetry.addData("blocc", color.hasFreight());
             telemetry.update();
+
             if(color.hasFreight()) {
-                gamepad1.rumble(2);
+                hadFreight = true;
+                gamepad1.rumble(50, 50, 200);
+            }
+
+            if(hadFreight) {
+                if(!color.hasFreight()) {
+                    gamepad1.rumble(0, 0, 5);
+                    hadFreight = false;
+                }
             }
         }
     }
