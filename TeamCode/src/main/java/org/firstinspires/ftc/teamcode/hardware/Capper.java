9878eecb0c7006894e44f9668ce302230cb700c0
Steven Lei <46882137+stevensll@users.noreply.github.com>
2022-03-30 12:22:54 -0400

    Update Capper.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Capper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Capper.java
@@ -5,25 +5,32 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class Capper extends ServoMechanism{
     //capper servo pos
-    public static double grab = .125;
-    public static double capping = 1;
-    public static double idle = 0;
+    public static double GRAB = .15;
+    public static double CAPPING_UP = 0.5;
+    public static double CAPPING_DOWN = 0.4;
+    public static double IDLE = 0;
     Servo capper;
     @Override
     public void init(HardwareMap hwMap) {
         capper = hwMap.get(Servo.class, "capper");
     }
 
-    public void idle() {
-        capper.setPosition(idle);
+    public void reset() {
+        //lift.reset();
+        capper.setPosition(IDLE);
     }
 
     public void grabCap() {
-        capper.setPosition(grab);
+        capper.setPosition(GRAB);
     }
 
-    public void cap() {
-        capper.setPosition(capping);
+    public void raise() {
+        capper.setPosition(CAPPING_UP);
+        //lift.setPos(7.5);
+    }
+    public void release(){
+        capper.setPosition(CAPPING_DOWN);
+        
     }
     @Override
     public void run(boolean bool) {
