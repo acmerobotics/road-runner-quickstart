32ccaf605971b5e13127b76dd7a6d9f62e3c53f9
Jacob Van Meter <jacobvm04@gmail.com>
2022-10-11 19:27:29 -0400

    Arm slide code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Subsystems/Arm.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode.Robot.Subsystems;
+
+import com.ThermalEquilibrium.homeostasis.Controllers.Feedback.FeedbackController;
+import com.ThermalEquilibrium.homeostasis.Controllers.Feedback.NoFeedback;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.CommandFramework.Subsystem;
+
+public class Arm extends Subsystem {
+
+    protected DcMotorEx slideLeft;
+    protected DcMotorEx slideRight;
+
+    protected double slideSetpoint = SlidePositions.down;
+
+    protected FeedbackController slideControllerLeft = new NoFeedback();
+    protected FeedbackController slideControllerRight = new NoFeedback();
+
+    public static class SlidePositions {
+        public static double down = 0;
+        public static double low = 1;
+        public static double high = 2;
+    }
+
+    private void commonInit(HardwareMap hwMap) {
+        slideLeft = hwMap.get(DcMotorEx.class, "slide_left");
+        slideRight = hwMap.get(DcMotorEx.class, "slide_right");
+    }
+
+    @Override
+    public void initAuto(HardwareMap hwMap) {
+        commonInit(hwMap);
+
+        slideLeft.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        slideRight.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    @Override
+    public void initTeleop(HardwareMap hwMap) {
+        commonInit(hwMap);
+    }
+
+    @Override
+    public void periodic() {
+        // check this method for getting the position lol it's wrong
+        double leftPower = slideControllerLeft.calculate(slideSetpoint, slideLeft.getCurrentPosition());
+        double rightPower = slideControllerRight.calculate(slideSetpoint, slideRight.getCurrentPosition());
+
+        slideLeft.setPower(leftPower);
+        slideRight.setPower(rightPower);
+    }
+
+    @Override
+    public void shutdown() {
+        slideLeft.setPower(0);
+        slideRight.setPower(0);
+    }
+}
