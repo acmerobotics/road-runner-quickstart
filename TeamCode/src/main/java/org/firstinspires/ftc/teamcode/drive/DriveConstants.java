0c224c9b335b1fcae138e90eebb07f6d3e8b0355
Nathan-Roach7 <naynayroach@icloud.com>
2022-10-12 13:37:19 -0500

    fixed kV value

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -53,7 +53,7 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.0195 / rpmToVelocity(MAX_RPM);
+    public static double kV = 0.0195;
     public static double kA = 0.0023;
     public static double kStatic = 0;
 
