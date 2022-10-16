95c4ef03d55019ea28bb0b0d7dbb298ec577843f
Ben Caunt <bdcaunt@gmail.com>
2022-10-16 02:57:27 -0400

    added method to convert slide encoder ticks to inches

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Subsystems/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Subsystems/Arm.java
@@ -2,12 +2,14 @@ package org.firstinspires.ftc.teamcode.Robot.Subsystems;
 
 import com.ThermalEquilibrium.homeostasis.Controllers.Feedback.FeedbackController;
 import com.ThermalEquilibrium.homeostasis.Controllers.Feedback.NoFeedback;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.CommandFramework.Subsystem;
 
 public class Arm extends Subsystem {
+
 
     protected DcMotorEx slideLeft;
     protected DcMotorEx slideRight;
@@ -26,6 +28,8 @@ public class Arm extends Subsystem {
     private void commonInit(HardwareMap hwMap) {
         slideLeft = hwMap.get(DcMotorEx.class, "slide_left");
         slideRight = hwMap.get(DcMotorEx.class, "slide_right");
+        slideLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        slideRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
     @Override
@@ -34,6 +38,8 @@ public class Arm extends Subsystem {
 
         slideLeft.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
         slideRight.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        slideLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        slideRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
     @Override
@@ -44,8 +50,11 @@ public class Arm extends Subsystem {
     @Override
     public void periodic() {
         // check this method for getting the position lol it's wrong
-        double leftPower = slideControllerLeft.calculate(slideSetpoint, slideLeft.getCurrentPosition());
-        double rightPower = slideControllerRight.calculate(slideSetpoint, slideRight.getCurrentPosition());
+        double leftPower = slideControllerLeft.calculate(slideSetpoint,
+                encoderTicksToInches(slideLeft.getCurrentPosition()));
+
+        double rightPower = slideControllerRight.calculate(slideSetpoint,
+                encoderTicksToInches(slideRight.getCurrentPosition()));
 
         slideLeft.setPower(leftPower);
         slideRight.setPower(rightPower);
@@ -55,5 +64,12 @@ public class Arm extends Subsystem {
     public void shutdown() {
         slideLeft.setPower(0);
         slideRight.setPower(0);
+    }
+
+    public static double encoderTicksToInches(double ticks) {
+        double SPOOL_SIZE_IN = 1.270 / 2.0;
+        double GEAR_RATIO = 13.7;
+        double TICKS_PER_REV = 28;
+        return SPOOL_SIZE_IN * 2 * Math.PI * GEAR_RATIO * ticks / TICKS_PER_REV;
     }
 }
