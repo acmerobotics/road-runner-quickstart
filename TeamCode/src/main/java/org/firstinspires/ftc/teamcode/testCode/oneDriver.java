4abff1fcae172bb1cd87215e506217007d23bd19
Ruiming Kang <113479175+KANGRUIMING@users.noreply.github.com>
2022-10-17 10:55:58 -0500

    Adjust Speed in TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testCode/oneDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testCode/oneDriver.java
@@ -156,7 +156,7 @@ public class oneDriver extends LinearOpMode {
             max = Math.max(max, Math.abs(leftBackPower));
             max = Math.max(max, Math.abs(rightBackPower));
 
-            if (max > 1.0) {
+            if (max > 0.6) {
                 leftFrontPower  /= max;
                 rightFrontPower /= max;
                 leftBackPower   /= max;
