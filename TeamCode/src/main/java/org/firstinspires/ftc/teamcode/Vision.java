77a7ffc9dcc19254904f83b63a4701196deaa4c3
harry.chen <harryxtchen@gmail.com>
2022-10-19 19:43:22 -0400

    updated vision

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision.java
@@ -1,32 +1,41 @@
 package org.firstinspires.ftc.teamcode;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
+import org.opencv.core.*;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
 
 public class Vision extends OpenCvPipeline {
 
+    // percentage color threshold
+    private static final double percentColorThreshold = 0.1;
+
     // green signal sleeve hsv values
-    private static final Scalar lowGreenSignalHSV = new Scalar(23, 50, 70);
-    private static final Scalar highGreenSignalHSV = new Scalar(32, 255, 255);
+    private static final Scalar lowGreenSignalHSV = new Scalar(143, 255, 84);
+    private static final Scalar highGreenSignalHSV = new Scalar(143, 255, 255);
     //pink signal sleeve hsv values
-    private static final Scalar lowPinkSignalHSV = new Scalar(23, 50, 70);
-    private static final Scalar highPinkSignalHSV = new Scalar(32, 255, 255);
+    private static final Scalar lowPinkSignalHSV = new Scalar(307, 255, 204);
+    private static final Scalar highPinkSignalHSV = new Scalar(307, 255, 255);
     //purple signal sleeve hsv values
-    private static final Scalar lowPurpleSignalHSV = new Scalar(23, 50, 70);
-    private static final Scalar highPurpleSignalHSV = new Scalar(32, 255, 255);
-
-
+    private static final Scalar lowPurpleSignalHSV = new Scalar(282, 255, 153);
+    private static final Scalar highPurpleSignalHSV = new Scalar(282, 255, 255);
 
     private final Telemetry telemetry;
 
+    private SleeveDirection sleeveDirection;
+
     public Vision(final Telemetry telemetry) {
         this.telemetry = telemetry;
+    }
+
+    public enum SleeveDirection {
+        LEFT,
+        CENTER,
+        RIGHT
+    }
+
+    public SleeveDirection getSleeveDirection() {
+        return sleeveDirection;
     }
 
     @Override
@@ -48,9 +57,34 @@ public class Vision extends OpenCvPipeline {
         //returning a grayscale image of what matches hsv ranges
         Imgproc.cvtColor(lowerHalfMatrix, lowerHalfHSV, Imgproc.COLOR_RGB2HSV);
 
+        final Mat greenMatrix = new Mat();
+        final Mat pinkMatrix = new Mat();
+        final Mat purpleMatrix = new Mat();
 
+        Core.inRange(lowerHalfHSV, lowGreenSignalHSV, highGreenSignalHSV, greenMatrix);
+        Core.inRange(lowerHalfHSV, lowPinkSignalHSV, highPinkSignalHSV, pinkMatrix);
+        Core.inRange(lowerHalfHSV, lowPurpleSignalHSV, highPurpleSignalHSV, purpleMatrix);
 
-        return null;
+        final double greenPercentage = Core.sumElems(greenMatrix).val[0] / HALF_RECT.area() / 255;
+        final double pinkPercentage = Core.sumElems(pinkMatrix).val[0] / HALF_RECT.area() / 255;
+        final double purplePercentage = Core.sumElems(purpleMatrix).val[0] / HALF_RECT.area() / 255;
+
+        final double maxPercentage = Math.max(Math.max(greenPercentage, pinkPercentage), purplePercentage);
+        if (maxPercentage < percentColorThreshold)
+            return inputMatrix;
+        else {
+            if (Double.compare(greenPercentage, maxPercentage) == 0)
+                sleeveDirection = SleeveDirection.LEFT;
+            else if (Double.compare(pinkPercentage, maxPercentage) == 0)
+                sleeveDirection = SleeveDirection.CENTER;
+            else
+                sleeveDirection = SleeveDirection.RIGHT;
+        }
+
+        telemetry.addData("SleeveDirection", (Object)sleeveDirection);
+        telemetry.update();
+
+        return inputMatrix;
 
     }
 
