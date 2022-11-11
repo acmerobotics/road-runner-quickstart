aeb0a7d16f3324f2aed9e8920c92d76e9177fabc
Ruiming Kang <113479175+KANGRUIMING@users.noreply.github.com>
2022-11-11 16:27:14 -0600

    17348

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/newCode/testOne.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/newCode/testOne.java
@@ -2,6 +2,7 @@
 
 package org.firstinspires.ftc.teamcode.newCode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -21,7 +22,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 import java.util.ArrayList;
 
-@TeleOp
+@Autonomous
 public class testOne extends LinearOpMode
 {
     OpenCvCamera camera;
