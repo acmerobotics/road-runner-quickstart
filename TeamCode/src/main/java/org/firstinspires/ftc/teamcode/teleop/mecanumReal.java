9c050115b79bde65c899a8eee412bce87b28d981
Sam948-byte <techguy763@gmail.com>
2023-01-17 18:57:17 -0600

    maybe fix field-oriented?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/mecanumReal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/mecanumReal.java
@@ -186,32 +186,24 @@ public class mecanumReal extends LinearOpMode {
         servoCommand.clawOpen(clawLeft, clawRight);
       }
 
+      double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+      double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+      double rx = gamepad1.right_stick_x;
 
-
-      // Setup a variable for each drive wheel to save power level for telemetry
-      double frontLeftPower;
-      double frontRightPower;
-      double backLeftPower;
-      double backRightPower;
-
-      // set gamepad values
-      double x = gamepad1.left_stick_x * 1.1;
-      double y = -gamepad1.left_stick_y;
-      double t = gamepad1.right_stick_x;
+      // Read inverse IMU heading, as the IMU heading is CW positive
       double botHeading = -imu.getAngularOrientation().firstAngle;
 
-      // rotation
-      double x_rotated =
-        x * Math.cos(botHeading) - y * Math.sin(botHeading);
-      double y_rotated =
-        x * Math.sin(botHeading) + y * Math.cos(botHeading);
+      double rotX = x * Math.cos(botHeading) - y * Math.sin(botHeading);
+      double rotY = x * Math.sin(botHeading) + y * Math.cos(botHeading);
 
-      // x, y, theta input mixing
-      double denominator = Math.max(Math.abs(y_rotated) + Math.abs(x_rotated) + Math.abs(t), 1);
-      frontLeftPower = (y + x + t) / denominator;
-      backLeftPower = (y - x + t) / denominator;
-      frontRightPower = (y - x - t) / denominator;
-      backRightPower = (y + x - t) / denominator;
+      // Denominator is the largest motor power (absolute value) or 1
+      // This ensures all the powers maintain the same ratio, but only when
+      // at least one is out of the range [-1, 1]
+      double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+      double frontLeftPower = (rotY + rotX + rx) / denominator;
+      double backLeftPower = (rotY - rotX + rx) / denominator;
+      double frontRightPower = (rotY - rotX - rx) / denominator;
+      double backRightPower = (rotY + rotX - rx) / denominator;
 
       // Send calculated power to motors
       if (gamepad1.left_bumper) {
