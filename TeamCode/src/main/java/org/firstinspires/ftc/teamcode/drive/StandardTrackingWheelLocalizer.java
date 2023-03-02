3d7ae84912150fbe36f0151d667798e0a1635b40
ftc21180 <ftc21180@gmail.com>
2023-03-02 13:51:44 -0600

    move dead wheel cable to control hub.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -51,12 +51,12 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
         lastEncPositions = lastTrackingEncPositions;
         lastEncVels = lastTrackingEncVels;
 
-        leftEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "parallelEncoder"));
+        leftEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "FrontRight"));
         rightEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "BackRight"));
-        frontEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "perpendicularEncoder"));
+        frontEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "FrontLeft"));
 
         // TODO: reverse any encoders using Encoder.setDirection(Encoder.Direction.REVERSE)
-        frontEncoder.setDirection(Encoder.Direction.REVERSE);
+        leftEncoder.setDirection(Encoder.Direction.REVERSE);
     }
 
     public static double encoderTicksToInches(double ticks) {
