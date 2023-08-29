e7ee954c10dbf4a0118981336b90ce094385c07f
Ryan Brott <rcbrott@gmail.com>
2023-08-28 18:08:03 -0700

    Add missing import

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/trajectorysequence/TrajectorySequenceBuilder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/trajectorysequence/TrajectorySequenceBuilder.java
@@ -26,6 +26,7 @@ import org.firstinspires.ftc.teamcode.trajectorysequence.sequencesegment.WaitSeg
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class TrajectorySequenceBuilder {
