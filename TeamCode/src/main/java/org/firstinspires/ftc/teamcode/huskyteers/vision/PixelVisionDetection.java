4b4aa5e800d4e5d53393f7b949cab7c617898da2
Ethan Henry <79533577+ethansocal@users.noreply.github.com>
2023-09-18 15:56:54 -0700

    Create tfod processor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/huskyteers/vision/PixelVisionDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/huskyteers/vision/PixelVisionDetection.java
@@ -1,4 +1,33 @@
 package org.firstinspires.ftc.teamcode.huskyteers.vision;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.ConceptTensorFlowObjectDetection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.firstinspires.ftc.vision.tfod.TfodProcessor;
+
 public class PixelVisionDetection {
+    private VisionPortal visionPortal;
+    private HardwareMap hwMap;
+    private TfodProcessor tfod;
+
+    public PixelVisionDetection(HardwareMap hwMap){
+        this.hwMap = hwMap;
+
+        init();
+    }
+
+
+    private void init() {
+        tfod = new TfodProcessor.Builder()
+                .build();
+
+        visionPortal = new VisionPortal.Builder()
+                .setCamera(hwMap.get(WebcamName.class, "Webcam 1"))
+                .addProcessor(tfod)
+                .build();
+    }
+
 }
