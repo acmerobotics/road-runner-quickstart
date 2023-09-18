3498ef040b0f2be53faa07fd0e1d6987c4a83b17
Ethan Henry <79533577+ethansocal@users.noreply.github.com>
2023-09-18 16:09:12 -0700

    Add tfod telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/huskyteers/vision/PixelVisionDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/huskyteers/vision/PixelVisionDetection.java
@@ -3,10 +3,14 @@ package org.firstinspires.ftc.teamcode.huskyteers.vision;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcontroller.external.samples.ConceptTensorFlowObjectDetection;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 import org.firstinspires.ftc.vision.tfod.TfodProcessor;
+
+import java.util.List;
 
 public class PixelVisionDetection {
     private VisionPortal visionPortal;
@@ -18,6 +22,24 @@ public class PixelVisionDetection {
 
         init();
     }
+
+    private void telemetryTfod(Telemetry telemetry) {
+
+        List<Recognition> currentRecognitions = tfod.getRecognitions();
+        telemetry.addData("# Objects Detected", currentRecognitions.size());
+
+        // Step through the list of recognitions and display info for each one.
+        for (Recognition recognition : currentRecognitions) {
+            double x = (recognition.getLeft() + recognition.getRight()) / 2 ;
+            double y = (recognition.getTop()  + recognition.getBottom()) / 2 ;
+
+            telemetry.addData(""," ");
+            telemetry.addData("Image", "%s (%.0f %% Conf.)", recognition.getLabel(), recognition.getConfidence() * 100);
+            telemetry.addData("- Position", "%.0f / %.0f", x, y);
+            telemetry.addData("- Size", "%.0f x %.0f", recognition.getWidth(), recognition.getHeight());
+        }   // end for() loop
+
+    }   // end method telemetryTfod()
 
 
     private void init() {
