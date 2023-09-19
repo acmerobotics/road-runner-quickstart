7db1325f8d15cb775dddf59ddf2efc5c00b79281
dancingsquirrel06 <118295532+dancingsquirrel06@users.noreply.github.com>
2023-09-18 17:13:46 -0700

    fixed null error

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/huskyteers/HuskyTeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/huskyteers/HuskyTeleOpMode.java
@@ -10,7 +10,7 @@ import com.qualcomm.robotcore.util.Range;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 
 @Config
-@TeleOp(name = "Husky TeleOp Mode" , group = "Teleop")
+@TeleOp(name = "Husky TeleOp Mode", group = "Teleop")
 public class HuskyTeleOpMode extends LinearOpMode {
     @Override
     public void runOpMode() {
@@ -32,18 +32,25 @@ public class HuskyTeleOpMode extends LinearOpMode {
             currentGamepad2 = gamepad2;
 
             PoseVelocity2d pw = huskyBot.alignWithAprilTag(1);
-            huskyBot.driveRobot(pw.component1().y, pw.component1().x, pw.component2(), 1.0);
+            huskyBot.driveRobot(pw.component1().y, pw.component1().x, -pw.component2(), 1.0);
 
             telemetry.addData("Drive", pw.component1().y);
             telemetry.addData("Strafe", pw.component1().x);
             telemetry.addData("Turn", pw.component2());
 
-            if(huskyBot.huskyVision.backdropAprilTagDetection.closestAprilTag() != null)
-                telemetry.addData("Closest April Tag Range", huskyBot.huskyVision.backdropAprilTagDetection.closestAprilTag().ftcPose.range);
-
-            if(huskyBot.huskyVision.backdropAprilTagDetection.getAprilTagById(1) != null )
-                telemetry.addData("April Tag ID 1:", huskyBot.huskyVision.backdropAprilTagDetection.getAprilTagById(1).ftcPose.range);
-
+            if (huskyBot.huskyVision.backdropAprilTagDetection.closestAprilTag() != null) {
+                if (huskyBot.huskyVision.backdropAprilTagDetection.closestAprilTag().ftcPose == null) {
+                    telemetry.addData("Error: ", "April tag has no pose");
+                } else {
+                    telemetry.addData("Closest April Tag Range", huskyBot.huskyVision.backdropAprilTagDetection.closestAprilTag().ftcPose.range);
+                }
+            }
+            if (huskyBot.huskyVision.backdropAprilTagDetection.getAprilTagById(1) != null) {
+                if (huskyBot.huskyVision.backdropAprilTagDetection.closestAprilTag().ftcPose == null) {
+                    telemetry.addData("Error: ", "April tag has no pose");
+                } else
+                    telemetry.addData("April Tag ID 1:", huskyBot.huskyVision.backdropAprilTagDetection.getAprilTagById(1).ftcPose.range);
+            }
             telemetry.update();
         }
         // endregion
