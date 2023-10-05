c741496442ca98c31bc76d26b4f50c615d0000f7
0zgeada <uzmanoa@gmail.com>
2023-10-04 20:48:05 -0400

    drafted an auto test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
@@ -1,20 +1,34 @@
 package org.firstinspires.ftc.teamcode;
 
+import static com.acmerobotics.roadrunner.ftc.Actions.runBlocking;
+
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Trajectory;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-@Autonomous
-public class AutoTest extends BrainSTEMOpMode {
+@Config
+@Autonomous (name="Robot: Auto Test--BE CAREFUL!!", group="Robot")
+public class AutoTest extends LinearOpMode {
+
+    MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
+    public Action trajectory =
+            drive.actionBuilder(drive.pose)
+                    .lineToYConstantHeading(20)   //TODO: lineTo vs splineTo??
+                    .turn(Math.PI / 2)
+                    .lineToX(10)
+
+                    .build();
+    //TODO: lineToConstantHeading vs lineTo?
 
     public void runOpMode() throws InterruptedException {
-        TrajectoryActionBuilder trajectory = new TrajectoryActionBuilder;  //TODO: why is there an error??
-        trajectory                //TODO: is this what I'm to do if there are multiple trajectories?
-               .lineToXConstantHeading(24)
-               .splineTo(new Vector2d(36,0), Math.PI)   //TODO: lineTo vs splineTo??
-               .turn(Math.PI / 2)
-               .lineToY(48);                //TODO: lineToConstantHeading vs lineTo?
+
+        waitForStart();
+
+        runBlocking(trajectory); //TODO: make brainSTEM robot class
     }
 }
