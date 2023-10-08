b3ac7cf07196c5729a379a188e9d453d8a8e58b7
Ozge Ada <uzmanoa@gmail.com>
2023-10-08 13:45:24 -0400

    Revert "drafted an auto test"
    This reverts commit c741496442ca98c31bc76d26b4f50c615d0000f7.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
@@ -1,34 +1,20 @@
 package org.firstinspires.ftc.teamcode;
 
-import static com.acmerobotics.roadrunner.ftc.Actions.runBlocking;
-
-import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.Action;
-import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Trajectory;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-@Config
-@Autonomous (name="Robot: Auto Test--BE CAREFUL!!", group="Robot")
-public class AutoTest extends LinearOpMode {
-
-    MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
-    public Action trajectory =
-            drive.actionBuilder(drive.pose)
-                    .lineToYConstantHeading(20)   //TODO: lineTo vs splineTo??
-                    .turn(Math.PI / 2)
-                    .lineToX(10)
-
-                    .build();
-    //TODO: lineToConstantHeading vs lineTo?
+@Autonomous
+public class AutoTest extends BrainSTEMOpMode {
 
     public void runOpMode() throws InterruptedException {
-
-        waitForStart();
-
-        runBlocking(trajectory); //TODO: make brainSTEM robot class
+        TrajectoryActionBuilder trajectory = new TrajectoryActionBuilder;  //TODO: why is there an error??
+        trajectory                //TODO: is this what I'm to do if there are multiple trajectories?
+               .lineToXConstantHeading(24)
+               .splineTo(new Vector2d(36,0), Math.PI)   //TODO: lineTo vs splineTo??
+               .turn(Math.PI / 2)
+               .lineToY(48);                //TODO: lineToConstantHeading vs lineTo?
     }
 }
