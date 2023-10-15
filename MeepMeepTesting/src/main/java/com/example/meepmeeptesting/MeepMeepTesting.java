9fa3147b71b65e8abb48d7a03a8b1e80c805d6fb
Ozge Ada <uzmanoa@gmail.com>
2023-10-14 22:15:55 -0400

    Adding MeepMeep for RR 1.0

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -0,0 +1,80 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class MeepMeepTesting {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+/********* Example **********
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
+                .lineToX(20)
+                .turn(Math.toRadians(90))
+                .lineToY(30)
+                .turn(Math.toRadians(90))
+                .lineToX(0)
+                .turn(Math.toRadians(90))
+                .lineToY(0)
+                .turn(Math.toRadians(90))
+                .build());
+*******************************/
+
+        Action trajectory =
+                myBot.getDrive().actionBuilder(new Pose2d(-36,-61,Math.toRadians(90)))
+
+                        // CENTER PLACEMENT
+
+                        // Replace prop with your yellow pixel (just push)
+                        .lineToY(-33)
+                        .waitSeconds(1)
+
+                        // Goto Backdrop to place your purple pixel
+                        .setReversed(true)
+                        .splineToLinearHeading(new Pose2d(-36,-36,Math.toRadians(0)),Math.toRadians(0))
+                        .lineToX(48)
+                        .waitSeconds(1)
+
+                        // Goto stack and collect 2 white pixels
+                        .setReversed(true)
+                        .setTangent(Math.toRadians(180))
+                        .splineTo(new Vector2d(12, -12),Math.toRadians(180))
+                        .lineToX(-61)
+                        .waitSeconds(1)
+
+                        // Goto Backstage and drop 2 white pixels
+                        .setReversed(false)
+                        .lineToX(12)
+                        .setTangent(Math.toRadians(0))
+                        .splineTo(new Vector2d(48, -36),Math.toRadians(0))
+                        .waitSeconds(1)
+
+                        // Goto alliance partner's robot and collect their pixels
+
+                        // Drop their yellow pixel to the correct spike
+
+                        // Place their purple pixel to the Backdrop (same location as yours)
+
+                        // Park
+
+                        .build();
+
+        myBot.runAction(trajectory);
+
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_OFFICIAL)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
