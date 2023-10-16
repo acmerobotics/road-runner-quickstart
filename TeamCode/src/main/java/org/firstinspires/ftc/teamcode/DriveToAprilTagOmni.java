8480518e5db300a3991ab84e66cb7b1e603440c9
FTC Team 21180 Error418 <108445260+EBError418@users.noreply.github.com>
2023-10-16 09:48:20 -0500

    enable DriveToAprilTagOmi for example

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveToAprilTagOmni.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveToAprilTagOmni.java
@@ -86,7 +86,7 @@ import java.util.concurrent.TimeUnit;
  */
 
 @TeleOp(name="Omni Drive To AprilTag", group = "Concept")
-@Disabled
+//@Disabled
 public class DriveToAprilTagOmni extends LinearOpMode
 {
     // Adjust these numbers to suit your robot.
