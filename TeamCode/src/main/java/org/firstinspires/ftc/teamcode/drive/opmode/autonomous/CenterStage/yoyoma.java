8b7a26ccd2c9e1d1ddfe04e4d364e990b45452e0
BruhGrums <m1246114@moreheadstate.edu>
2023-12-01 21:54:02 -0500

    NATTALIE do u see this

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/autonomous/CenterStage/yoyoma.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.drive.opmode.autonomous.CenterStage;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+@Config
+@Autonomous
+
+public class yoyoma extends LinearOpMode {
+    
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
