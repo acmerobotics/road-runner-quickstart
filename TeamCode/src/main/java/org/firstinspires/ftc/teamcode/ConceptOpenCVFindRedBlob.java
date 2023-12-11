2bbf30e369fb17ddf649f99c6db671172d910635
vector C <vector5233@gmail.com>
2023-12-11 07:03:39 -0500

    Fixed syntax errors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptOpenCVFindRedBlob.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptOpenCVFindRedBlob.java
@@ -1,9 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.Size;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
@@ -21,55 +25,70 @@ import org.opencv.imgproc.Imgproc;
 @Autonomous(name = "ConceptOpenCVFindRedBlob", group= "Concept")
 public class ConceptOpenCVFindRedBlob extends LinearOpMode {
 
-    WebcamName webcamName= hardwareMap.get(WebcamName.class, "Webcam 1");
+    WebcamName webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
     VisionPortal visionPortal;
     OpenCVProcessor visionProcessor;
-    final int RESOLUTION_WIDTH= 640;
-    final int RESOLUTION_HEIGHT=480;
+    final static int RESOLUTION_WIDTH = 640;
+    final static int RESOLUTION_HEIGHT = 480;
 
     public static int LW_LEFT = 2;
     public static int LW_TOP = 2;
-    public static int LW_WIDTH = RESOLUTION_WIDTH/2 - 4;
+    public static int LW_WIDTH = RESOLUTION_WIDTH / 2 - 4;
     public static int LW_HEIGHT = RESOLUTION_HEIGHT - 2;
 
-    public static int RW_LEFT = RESOLUTION_WIDTH/2 + 2;
+    public static int RW_LEFT = RESOLUTION_WIDTH / 2 + 2;
     public static int RW_TOP = 2;
-    public static int RW_WIDTH = RESOLUTION_WIDTH/2 - 4;
+    public static int RW_WIDTH = RESOLUTION_WIDTH / 2 - 4;
     public static int RW_HEIGHT = RESOLUTION_HEIGHT - 2;
 
-  
+    public static double THRESHOLD = 5.0;
+
+
     //Rect uses left x, top y, width, height in camera coodinates
-    Rect leftWindow, rightWindow; 
-	
+    Rect leftWindow, rightWindow;
+
     public void runOpMode() {
-	telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
         visionProcessor = new OpenCVProcessor();
         visionPortal = new VisionPortal.Builder()
-			    .setCamera(webcamName)
+                .setCamera(webcamName)
                 .addProcessors((VisionProcessor) visionProcessor)
                 .setCameraResolution(new Size(RESOLUTION_WIDTH, RESOLUTION_HEIGHT))
                 .build();
         while (!isStarted() && !isStopRequested()) {
+            telemetry.addLine("Hit Y to see input frame, A to see output frame");
             telemetry.addData("Red blob on side: ", visionProcessor.getSide());
-	    telemetry.addData("mean red on left side: ", visionProcessor.leftMean);
-	    telemetry.addData("mean red on right side: ", visionProcessor.rightMean);
-	    telemetry.addData("THRESHOLD: ", THRESHOLD);
+            telemetry.addData("mean red on left side: ", visionProcessor.leftMean);
+            telemetry.addData("mean red on right side: ", visionProcessor.rightMean);
+            telemetry.addData("THRESHOLD: ", THRESHOLD);
+            if (gamepad1.y) {
+                visionProcessor.setView(visionProcessor.INPUT);
+            }
+            else if (gamepad1.a) {
+                visionProcessor.setView(visionProcessor.OUTPUT);
+            }
         }
     }
-
 
 
     class OpenCVProcessor implements VisionProcessor {
 
         public final int BLUE_CHANNEL = 1;
         public final int RED_CHANNEL = 2;
-        public String side = "None";  			// accessible side ID
-        public float leftMean, rightMean; 		// accessible mean red metrics
+        public String side = "None";            // accessible side ID
+        public double leftMean, rightMean;        // accessible mean red metrics
         public final int THRESHOLD = 5;   // sets sensitivity of detector.  
-	    				// if the mean red of one side is greater than THRESHOLD + mean of the other, that side is "Red"
+        // if the mean red of one side is greater than THRESHOLD + mean of the other, that side is "Red"
 
+        public Boolean INPUT = true;
+        public Boolean OUTPUT = false;
+        Boolean inOut = INPUT;
         public String getSide() {
-        	return side;
+            return side;
+        }
+
+        public void setView(Boolean inOut) {
+            this.inOut = inOut;
         }
 
         @java.lang.Override
@@ -83,59 +102,65 @@ public class ConceptOpenCVFindRedBlob extends LinearOpMode {
                 return null;
             }
             Mat workingMat = new Mat();
-	    leftWindow = new Rect(LW_LEFT, LW_TOP, LW_WIDTH, LW_HEIGHT);
-	    rightWindow = new Rect(RW_LEFT, RW_TOP, RW_WIDTH, RW_HEIGHT);
-            Imgproc.cvtColor(frame, workingMat, Imgproc.RGB2YCrCb);   // Possibly: Use HSV.  Use inRange() to convert to binary matrix.
+            leftWindow = new Rect(LW_LEFT, LW_TOP, LW_WIDTH, LW_HEIGHT);
+            rightWindow = new Rect(RW_LEFT, RW_TOP, RW_WIDTH, RW_HEIGHT);
+            Imgproc.cvtColor(frame, workingMat, Imgproc.COLOR_BGR2YCrCb);   // Possibly: Use HSV.  Use inRange() to convert to binary matrix.
             Mat leftCrop = workingMat.submat(leftWindow);
             Core.extractChannel(leftCrop, leftCrop, RED_CHANNEL);
 
-            rightCrop = workingMat.submat(rightWindow);
+            Mat rightCrop = workingMat.submat(rightWindow);
             Core.extractChannel(rightCrop, rightCrop, RED_CHANNEL);
 
             leftMean = Core.mean(leftCrop).val[0];
             rightMean = Core.mean(rightCrop).val[0];
 
             if (leftMean - rightMean > THRESHOLD) {
-                side = “Left”;
+                side = "Left";
+            } else if (rightMean - leftMean > THRESHOLD) {
+                side = "Right";
+            } else {
+                side = "None";
             }
-            else if (rightMean - leftMean > THRESHOLD) {
-                side = “Right”;
-            }
-            else {side = “None”;}
 
 // release all created matrices!  Otherwise a memory leak will occur.
-            workingMat.release();
             leftCrop.release();
             rightCrop.release();
-            return frame;
+            if (inOut) {
+                workingMat.release();
+                return frame;
+            }
+            else {
+                frame.release();
+                return workingMat;
+            }
         }
 
         @java.lang.Override
         public void onDrawFrame(Canvas canvas, int onscreenWidth, int onscreenHeight, float scaleBmpPxToCanvasPx, float scaleCanvasDensity, Object userContext) {
-		paint leftPaint = new Paint();
-            if side.equals(“Left”) {
+
+            Paint leftPaint = new Paint();
+            if (side.equals("Left")) {
                 leftPaint.setColor(Color.RED);
-            }
-	else {
+            } else {
                 leftPaint.setColor(Color.GREEN);
             }
-            leftPaint.setStyle(Paint.Style.STROKE); 38 leftPaint.setStrokeWidth(scaleCanvasDensity * 4);
-
+            leftPaint.setStyle(Paint.Style.STROKE);
+            leftPaint.setStrokeWidth(scaleCanvasDensity * 4);
             canvas.drawRect(makeGraphicsRect(leftWindow, scaleBmpPxToCanvasPx), leftPaint);
 
-            paint rightPaint = new Paint();
-            if side.equals(“Right”) {
+
+            Paint rightPaint = new Paint();
+            if (side.equals("Right")) {
                 rightPaint.setColor(Color.RED);
-            }
-	else {
+            } else {
                 rightPaint.setColor(Color.GREEN);
             }
-            rightPaint.setStyle(Paint.Style.STROKE); 38 rightPaint.setStrokeWidth(scaleCanvasDensity * 4);
+            rightPaint.setStyle(Paint.Style.STROKE);
+            rightPaint.setStrokeWidth(scaleCanvasDensity * 4);
 
             canvas.drawRect(makeGraphicsRect(rightWindow, scaleBmpPxToCanvasPx), rightPaint);
-        
+
         }
-        
 
 
 // convert a camera rectangle (x,y,width,height) into a screen rectangle
@@ -149,7 +174,8 @@ public class ConceptOpenCVFindRedBlob extends LinearOpMode {
 
             return new android.graphics.Rect(left, top, right, bottom);
         }
-
+    }
+}
 
 
 
