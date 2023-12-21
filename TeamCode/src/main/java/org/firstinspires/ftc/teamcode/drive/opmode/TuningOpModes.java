7d1abca05c91e4b1add3a33d78e845fb3e53fe21
Aldhanekaa <aldhanekadev@gmail.com>
2023-12-21 13:26:57 +0700

    add Drive interface for wide use in opmode tuning

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TuningOpModes.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.drive.opmode;
+
+import com.acmerobotics.roadrunner.drive.Drive;
+import com.acmerobotics.roadrunner.drive.TankDrive;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.drive.SampleTankDrive;
+
+public class TuningOpModes {
+
+    // TODO: change this to TankDrive.class if you're using tank
+    public static final Class DRIVE_CLASS = SampleMecanumDrive.class;
+
+}
