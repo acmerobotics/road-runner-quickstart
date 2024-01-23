05d1c7e5acc916b43655add11e80657021f32372
vector C <vector5233@gmail.com>
2024-01-23 09:39:38 -0500

    Added @Autonomous annotation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptGetPoseFromAT.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptGetPoseFromAT.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.PoseVelocity2d;
 import com.acmerobotics.roadrunner.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.RobotLog;
 
@@ -10,6 +11,7 @@ import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 
 import java.util.List;
 
+@Autonomous(name="ConceptGetPoseFromAT", group="testing")
 public class ConceptGetPoseFromAT extends LinearOpMode {
     AutoFluffy fluffy;
     List<AprilTagDetection> detections;
