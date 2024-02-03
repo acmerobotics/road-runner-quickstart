cf02637b8e579cc7ce3aac93dc5ed1f7fe3113cf
Ryan Triplett <Rymantheman@yahoo.com>
2024-02-03 12:17:36 -0500

    Ryan
    Ryan

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/autonomous/CenterStage/ryanTriplett.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/autonomous/CenterStage/ryanTriplett.java
@@ -12,14 +12,18 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.drive.DriveConstants;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.drive.opmode.visionCenterStage.Camerainitialization;
 import org.firstinspires.ftc.teamcode.drive.opmode.visionCenterStage.blueCameraPipeline;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvWebcam;
 
-@Autonomous(name="piped", group = "practice")
+@Autonomous(name="ryanTriplett", group = "practice")
 @Config
 public class ryanTriplett extends LinearOpMode {
     private final Pose2d startPose = new Pose2d(0, 0, Math.toRadians(90));
@@ -29,16 +33,42 @@ public class ryanTriplett extends LinearOpMode {
     private final Pose2d endPose3 = new Pose2d(15, 0, Math.toRadians(0));
 
     private final double travelSpeed = 45.0, travelAccel = 30.0;
+    private final int width = 1280, height = 720;
 
     SampleMecanumDrive drive;
 
+    // This is the Microsoft Life Cam 3000
+    OpenCvWebcam webcam1 = null;
+
     blueCameraPipeline ourCam = new blueCameraPipeline();
-    private blueCameraPipeline.MovementDirection linePlace;
+    blueCameraPipeline.MovementDirection linePlace;
+
 
     @Override
     public void runOpMode() throws InterruptedException {
 
         drive = new SampleMecanumDrive(hardwareMap);
+        // Set up the webcam
+        WebcamName adjustCameraName = hardwareMap.get(WebcamName.class, "Webcam 1");
+        // adjustCamera was the deviceName in last years code
+        // We may need to change the name adjustCamera to Webcam1
+        webcam1 = OpenCvCameraFactory.getInstance().createWebcam(adjustCameraName);
+
+        // Set the camera's pipeline
+        webcam1.setPipeline(ourCam);
+
+        // Open the camera
+        webcam1.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                webcam1.startStreaming(width, height, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+
+            }
+        });
 
         while (!isStarted()) {
             drive.initArm();
