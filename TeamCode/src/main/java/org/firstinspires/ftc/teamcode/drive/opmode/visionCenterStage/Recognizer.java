7b28f473e24c3e2c66197958deb7931da66985c6
keyoverlayfinder <jaxsonrb@gmail.com>
2024-02-14 11:21:34 -0500

    VISION SYSTEM WORKS

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/visionCenterStage/Recognizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/visionCenterStage/Recognizer.java
@@ -13,48 +13,38 @@ public class Recognizer extends OpenCvPipeline {
         LEFT,
         MIDDLE,
         RIGHT,
-        UNKNOWN
     }
-    public pixelLocation Location = pixelLocation.UNKNOWN;
-    public final Scalar blueLower = new Scalar(0.0, 0.0, 0.0);
-    public final Scalar blueUpper = new Scalar(255.0,255.0,255.0);
-
     private final Mat YcBcR = new Mat();
-    private final Mat colorMask = new Mat();
-    private final Mat leftMat = new Mat();
-    private final Mat middleMat = new Mat();
-    private final Mat rightMat = new Mat();
-    private final Mat unknownMat = new Mat();
+    private Mat leftMat = new Mat();
+    private Mat middleMat = new Mat();
+    private Mat rightMat = new Mat();
 
     private double leftAvg, middleAvg, rightAvg;
     private final Mat output = new Mat();
-    private final Rect leftRect = new Rect(0,0,420,720);
-    private final Rect middleRect = new Rect(420,0,440,720);
+    private final Rect leftRect = new Rect(0,0,440,720);
+    private final Rect middleRect = new Rect(440,0,420,720);
     private final Rect rightRect = new Rect(860,0,420,720);
 
     @Override
     public Mat processFrame(Mat input) {
-        Imgproc.cvtColor(input,YcBcR,Imgproc.COLOR_RGB2YCrCb);
+        Imgproc.cvtColor(input,YcBcR, Imgproc.COLOR_RGB2YCrCb);
 
-        Mat colorMask = new Mat();
-        Core.inRange(YcBcR, blueLower, blueUpper, colorMask);
+        leftMat = input.submat(leftRect);
+        Core.inRange(YcBcR, new Scalar(16.0, 0.0, 0.0), new Scalar(230, 128.0, 255.0), leftMat);
 
-        colorMask.copyTo(leftMat);
-        leftMat.submat(leftRect);
+        leftAvg = Core.mean(leftMat).val[0]*100000.0;
 
-        leftAvg = Core.mean(leftMat).val[0];
+        middleMat = input.submat(middleRect);
+        Core.inRange(YcBcR, new Scalar(16.0, 0.0, 0.0), new Scalar(230, 128.0, 255.0), middleMat);
 
-        colorMask.copyTo(middleMat);
-        middleMat.submat(middleRect);
+        middleAvg = Core.mean(middleMat).val[0]*100000.0;
 
-        middleAvg = Core.mean(middleMat).val[0];
+        rightMat = input.submat(rightRect);
+        Core.inRange(YcBcR, new Scalar(16.0, 0.0, 0.0), new Scalar(230, 128.0, 255.0), rightMat);
 
-        colorMask.copyTo(rightMat);
-        rightMat.submat(rightRect);
+        rightAvg = Core.mean(rightMat).val[0]*100000.0;
 
-        rightAvg = Core.mean(rightMat).val[0];
-
-        input.copyTo(output, colorMask);
+        input.copyTo(output);
 
         Imgproc.rectangle(output, leftRect, new Scalar(255.0, 0.0, 0.0), 2);
         Imgproc.rectangle(output, middleRect, new Scalar(255.0, 0.0, 0.0), 2);
@@ -67,12 +57,10 @@ public class Recognizer extends OpenCvPipeline {
     public pixelLocation getPixelLocation() {
         if (leftAvg > middleAvg && leftAvg > rightAvg) {
             return pixelLocation.LEFT;
-        } else if (middleAvg > leftAvg && middleAvg > rightAvg) {
-            return pixelLocation.MIDDLE;
         } else if (rightAvg > leftAvg && rightAvg > middleAvg) {
             return pixelLocation.RIGHT;
         } else {
-            return pixelLocation.UNKNOWN;
+            return pixelLocation.MIDDLE;
         }
     }
 }
