e561298675b0c246a4e7159ad8b55a9902f1e873
midK-night <130721380+midK-night@users.noreply.github.com>
2024-05-20 17:05:55 -0500

    test
    just don't pull this shi

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/test.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.tuning;
+
+public class test {
+    private static String hola;
+
+    public test() {
+        hola = "sup?\nThis shit works?";
+    }
+
+    public static String getHola() {
+        return hola;
+    }
+}
