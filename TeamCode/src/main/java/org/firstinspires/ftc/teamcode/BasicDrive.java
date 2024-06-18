9bdfa9f9b0c27acfd40adc06e4931aab53a24086
krishvadivel <125418482+krishv745@users.noreply.github.com>
2024-06-18 14:46:07 -0500

    Finalize BasicDrive.java
    fixes bugs with @s, adds the opMode loop code in relation to RobotDrive initialization

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicDrive.java
@@ -1,19 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.robot.Robot;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.IMU;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+@TeleOp
+public class BasicDrive extends LinearOpMode {
 
-public class BasicDrive {
     public RobotDrive bot;
 
     @Override
@@ -21,12 +18,21 @@ public class BasicDrive {
         bot = new RobotDrive();
         bot.init(hardwareMap);
 
-        telemtry.addData("Status", "Initialized");
-        telemtry.update();
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
         waitForStart();
 
         while (opModeIsActive()) {
+            double jx = -gamepad1.left_stick_y - gamepad1.right_stick_y;
+            double jy = -gamepad1.left_stick_x;
+            double jw = -gamepad1.right_stick_x;
 
+            double speed = (1.0 - gamepad1.left_trigger * 0.75);
+
+            bot.driveXYW(jx * speed, jy * speed, jw);
+
+            telemetry.addData("Status", "Running");
+            telemetry.update();
         }
     }
 }
