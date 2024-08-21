9b37a4f8f92c15d527c2b4e27a8c6d4616a2d613
RavenRain44 <89553123+RavenRain44@users.noreply.github.com>
2024-08-21 15:52:41 -0500

    Added folders and added Constants.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+
+public class Constants {
+    public static final double TICKS_PER_REV = 1;
+    public static final double MAX_RPM = 1;
+
+    public static final boolean RUN_USING_ENCODER = true;
+    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0, 0, 0,
+            getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV));
+
+    public static double WHEEL_RADIUS = 2; // in
+    public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
+    public static double TRACK_WIDTH = 1; // in
+
+
+}
