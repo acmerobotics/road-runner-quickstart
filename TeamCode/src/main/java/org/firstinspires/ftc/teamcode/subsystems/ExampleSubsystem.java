d2c586d6efa843db5fe15cab67675aac3e35f401
Sam <finepointdog@gmail.com>
2024-08-28 20:10:07 -0500

    file changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ExampleSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ExampleSubsystem.java
@@ -1,15 +1,18 @@
-package org.firstinspires.ftc.teamcode.subsystems;
+package org.firstinspires.ftc.teamcode.subsytems;
 
 import com.arcrobotics.ftclib.command.SubsystemBase;
 
 public class ExampleSubsystem extends SubsystemBase {
-
+    /**
+     * Creates a new ExampleSubsystem.
+     * <a href="https://docs.ftclib.org/ftclib/command-base/command-system/subsystems">...</a>
+     */
     public ExampleSubsystem() {
+
     }
 
     @Override
     public void periodic() {
+        // This method will be called once per scheduler run
     }
-
 }
-
