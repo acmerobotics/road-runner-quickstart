f2bb6a926d52237fc2878f3c73806711ef07df1a
theowong1 <theodorewong3@gmail.com>
2024-08-31 20:16:54 -0500

    Created Skeleton for Tele-Op and Autonomous

---
+++ CosmoboticsCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/DriveLogic.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+public class Drive {
+    public Drive(HardwareMap hardwareMap) {
+
+    }
+
+    public void update(Gamepad gamepad1) {
+
+    }
+}
---
+++ CosmoboticsCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TransportLogic.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.ServoImplEx;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+public class Transport {
+    public Transport(HardwareMap hardwareMap) {
+
+    }
+
+    public void update() {
+
+    }
+    public void update(Gamepad gamepad1, Gamepad gamepad2) {
+
+    }
+}
