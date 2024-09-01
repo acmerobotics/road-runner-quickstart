783c4b3c3ea97ff3f2f8303047a4f34088f88fb5
theowong1 <theodorewong3@gmail.com>
2024-08-31 20:18:00 -0500

    Created Skeleton for Autonomous and Tele-Op

--- CosmoboticsCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MiscLogic.java
+++ CosmoboticsCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MiscLogic.java
@@ -1,17 +1,9 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
-
-import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-public class Misc {
-    public Misc(HardwareMap hardwareMap) {
+public class MiscLogic {
+    public MiscLogic(HardwareMap hardwareMap) {
 
     }
 
-    public void update() {
-
-    }
-    public void update(Gamepad gamepad1, Gamepad gamepad2) {
-
-    }
 }
--- CosmoboticsCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TransportLogic.java
+++ CosmoboticsCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TransportLogic.java
@@ -1,22 +1,10 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
-
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.ServoImplEx;
-import com.qualcomm.robotcore.hardware.TouchSensor;
-public class Transport {
-    public Transport(HardwareMap hardwareMap) {
+
+public class TransportLogic {
+    public TransportLogic(HardwareMap hardwareMap) {
 
     }
 
-    public void update() {
-
-    }
-    public void update(Gamepad gamepad1, Gamepad gamepad2) {
-
-    }
 }
