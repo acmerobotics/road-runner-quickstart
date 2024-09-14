c0e11283fd927fc9b651a8cb08383375b0c02ca9
midK-night <pasum462@gmail.com>
2024-09-14 16:45:43 -0500

    implement tracking
    tracks RGB values and distance, with telemetry output

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/SensorREVColorDistance.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/SensorREVColorDistance.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.view.View;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColSensorNew.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColSensorNew.java
@@ -3,23 +3,22 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 
 @TeleOp
 public class ColSensorNew extends LinearOpMode {
 
     ColorSensor color;
-double redvalue;
-double bluevalue;
+    int redvalue; //raw color values
+    int bluevalue;
+    int greenvalue;
+    double distance; // largest when an object is close to the sensor, range of 0-2047
+    String currentColor;
 
-public void getColor() {
-    redvalue = colorSensor.red;
-    bluevalue = colorSensor.blue;
 
-    if(redvalue = colorSensor.red ){
-        System.out.println("Red");
-    }
-}
     @Override
     public void runOpMode() {
         // Get the color sensor from hardwareMap
@@ -30,9 +29,17 @@ public void getColor() {
 
         // While the Op Mode is running, update the telemetry values.
         while (opModeIsActive()) {
-            telemetry.addData("Red", color.red());
-            telemetry.addData("Green", color.green());
-            telemetry.addData("Blue", color.blue());
+            redvalue = color.red();
+            greenvalue = color.green();
+            bluevalue = color.blue();
+            distance = ((DistanceSensor) color).getDistance(DistanceUnit.CM);
+
+
+
+            telemetry.addData("Red: ", redvalue);
+            telemetry.addData("Green: ", greenvalue);
+            telemetry.addData("Blue: ", bluevalue);
+            telemetry.addData("Distance: ", distance);
             telemetry.update();
         }
     }
