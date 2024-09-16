e1b1d45794c584bcb59beb33cdd4b391a3944446
plutusmaximus <github@aidiastudios.com>
2024-09-16 11:10:09 -0700

    Cleaned up, refactored, added tests for angle values <= -2PI and >= 2PI

--- TeamCode/src/test/java/org/firstinspires/ftc/teamcode/New/SubSystems/AngleTest.kt
+++ TeamCode/src/test/java/org/firstinspires/ftc/teamcode/New/SubSystems/AngleTest.kt
@@ -4,16 +4,22 @@ import org.junit.Assert.*
 import org.firstinspires.ftc.teamcode.Kotlin_Bromine_Arya.*
 
 import org.junit.Test
+import java.util.Random
 
 class AngleTest {
 
     @Test
-    fun wrap() {
+    //Tests the wrap() function when the angle to wrap is within
+    //the range -PI to PI.
+    //Expected Outcome: Wrap should NOT occur.
+    fun wrap_inside_range() {
         val angle = Angle
         val incr = 0.01
         val epsilon = 0.0
 
         //Test values that SHOULD NOT wrap
+
+        //Test a bunch of values from -PI to PI.
         var expected = -Math.PI
         while(expected <= Math.PI){
             val actual = angle.wrap(expected)
@@ -22,17 +28,58 @@ class AngleTest {
 
             expected += incr
         }
+    }
 
-        //Test that values > Math.PI wrap
-        var testValue = Math.PI + 1
-        expected = testValue - Math.PI * 2
-        var actual = angle.wrap(testValue)
-        assertEquals(expected, actual, epsilon)
+    @Test
+    //Tests the wrap() function when the angle to wrap is outside
+    //the range -PI to PI.
+    //Expected Outcome: Wrap should occur.
+    fun wrap_outside_range() {
+        val angle = Angle
+        val epsilon = 0.0
 
-        //Test that values < -Math.PI wrap
-        testValue = -Math.PI - 1
-        expected = testValue + Math.PI * 2
-        actual = angle.wrap(testValue)
-        assertEquals(expected, actual, epsilon)
+        val rng = Random()
+
+        //Test that values > PI wrap
+        for(i in 1..100) {
+            val testValue = Math.PI + rng.nextDouble()
+            val expected = testValue - Math.PI * 2
+            val actual = angle.wrap(testValue)
+            assertEquals(expected, actual, epsilon)
+        }
+
+        //Test that values < -PI wrap
+        for(i in 1..100) {
+            val testValue = -Math.PI - rng.nextDouble()
+            val expected = testValue + Math.PI * 2
+            val actual = angle.wrap(testValue)
+            assertEquals(expected, actual, epsilon)
+        }
+    }
+
+    @Test
+    //Tests the wrap() function when the angle to wrap is >= 2PI or <= -2PI.
+    //Expected Outcome: Wrap should occur.
+    fun wrap_outside_range_large_values() {
+        val angle = Angle
+        val epsilon = 0.0
+
+        val rng = Random()
+
+        //Test that values > 2PI wrap
+        for(i in 1..100) {
+            val testValue = (2 * Math.PI) + (rng.nextDouble() * 100)
+            val expected = testValue - Math.PI * 2
+            val actual = angle.wrap(testValue)
+            assertEquals(expected, actual, epsilon)
+        }
+
+        //Test that values < -2PI wrap
+        for(i in 1..100) {
+            val testValue = -(2 * Math.PI) - (rng.nextDouble() * 100)
+            val expected = testValue + Math.PI * 2
+            val actual = angle.wrap(testValue)
+            assertEquals(expected, actual, epsilon)
+        }
     }
 }
