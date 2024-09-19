c3cf0774d52f36526ce8007f6b6e2c3862c53455
vedikavenugopal <155735931+vedikavenugopal@users.noreply.github.com>
2024-09-18 19:17:58 -0700

    test

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/BasicOmniOpMode_Linear.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/BasicOmniOpMode_Linear.java
@@ -26,7 +26,7 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
+// this is a test
 package org.firstinspires.ftc.robotcontroller.external.samples;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # Road Runner Quickstart
-
 Check out the [docs](https://rr.brott.dev/docs/v1-0/tuning/).
 
