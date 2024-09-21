e18143dea7581daa81e050ee2d35ff9a57c9cddc
masonc8419 <masonc3000@gmail.com>
2024-09-20 19:06:26 -0700

    added color sensor code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpp/TheTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpp/TheTeleOp.java
@@ -1,12 +1,19 @@
 package org.firstinspires.ftc.teamcode.TeleOpp;
 
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.SwitchableLight;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
@@ -54,6 +61,26 @@ public class TheTeleOp extends LinearOpMode {
         enum LiftState {LIFTSTART, LIFTEXTEND, LIFTARM, LIFTRELEASE, LIFTRETRACT}
         LiftState liftState = LiftState.LIFTSTART;
 
+        NormalizedColorSensor colorSensor;
+        View relativeLayout;
+
+        int relativeLayoutId = hardwareMap.appContext.getResources().getIdentifier("RelativeLayout", "id", hardwareMap.appContext.getPackageName());
+        relativeLayout = ((Activity) hardwareMap.appContext).findViewById(relativeLayoutId);
+        float gain = 2;
+
+        final float[] hsvValues = new float[3];
+
+        boolean xButtonPreviouslyPressed = false;
+        boolean xButtonCurrentlyPressed = false;
+
+        colorSensor = hardwareMap.get(NormalizedColorSensor.class, "sensor_color");
+
+        if (colorSensor instanceof SwitchableLight) {
+            ((SwitchableLight)colorSensor).enableLight(true);
+        }
+
+
+
         ElapsedTime timer = new ElapsedTime();
 
         waitForStart();
@@ -68,6 +95,52 @@ public class TheTeleOp extends LinearOpMode {
             if (gamepad1.options) {
                 imu.resetYaw();
             }
+
+            if (gamepad1.a) {
+                gain += 0.005;
+            } else if (gamepad1.b && gain > 1) {
+                gain -= 0.005;
+            }
+
+            telemetry.addData("Gain", gain);
+            colorSensor.setGain(gain);
+
+            xButtonCurrentlyPressed = gamepad1.x;
+
+            if (xButtonCurrentlyPressed != xButtonPreviouslyPressed) {
+                if (xButtonCurrentlyPressed) {
+                    if (colorSensor instanceof SwitchableLight) {
+                        SwitchableLight light = (SwitchableLight)colorSensor;
+                        light.enableLight(!light.isLightOn());
+                    }
+                }
+            }
+            xButtonPreviouslyPressed = xButtonCurrentlyPressed;
+
+            NormalizedRGBA colors = colorSensor.getNormalizedColors();
+
+            Color.colorToHSV(colors.toColor(), hsvValues);
+
+            telemetry.addLine()
+                    .addData("Red", "%.3f", colors.red)
+                    .addData("Green", "%.3f", colors.green)
+                    .addData("Blue", "%.3f", colors.blue);
+            telemetry.addLine()
+                    .addData("Hue", "%.3f", hsvValues[0])
+                    .addData("Saturation", "%.3f", hsvValues[1])
+                    .addData("Value", "%.3f", hsvValues[2]);
+            telemetry.addData("Alpha", "%.3f", colors.alpha);
+
+            if (colors.red > 225 && colors.green > 225 && colors.blue < 30) {
+                telemetry.addData("Block Color", "yellow");
+            } else if (colors.red > 225 && colors.green < 30 && colors.blue < 30) {
+                telemetry.addData("Block Color", "red");
+            } else if (colors.red < 30 && colors.green < 30 && colors.blue > 225) {
+                telemetry.addData("Block Color", "blue");
+            } else {
+                telemetry.addData("Block Color", "none");
+            }
+
 
             switch (liftState) {
                 case LIFTSTART:
@@ -169,6 +242,18 @@ public class TheTeleOp extends LinearOpMode {
 
 
             timer.reset();
-        }
+            telemetry.update();
+
+            relativeLayout.post(new Runnable() {
+                                    public void run() {
+                                        relativeLayout.setBackgroundColor(Color.HSVToColor(hsvValues));
+                                    }
+                                });
+        relativeLayout.post(new Runnable() {
+            public void run() {
+                relativeLayout.setBackgroundColor(Color.WHITE);
+            }
+        });
     }
+}
 }
