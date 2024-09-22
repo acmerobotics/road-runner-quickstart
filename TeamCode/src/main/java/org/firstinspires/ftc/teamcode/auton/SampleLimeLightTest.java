71780af00edc243e275252465a3990ea21834e5c
vik-j <114836259+vik-j@users.noreply.github.com>
2024-09-22 14:40:23 -0700

    added sample limelight code
    added sample limelight code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/SampleLimeLightTest.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.auton;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+
+public class SampleLimeLightTest extends LinearOpMode {
+    private Limelight3A limelight;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+
+        telemetry.setMsTransmissionInterval(11);
+
+        limelight.pipelineSwitch(0);
+
+        /*
+         * Starts polling for data.
+         */
+        limelight.start();
+
+        while (opModeIsActive()) {
+            LLResult result = limelight.getLatestResult();
+            if (result != null) {
+                if (result.isValid()) {
+                    Pose3D botpose = result.getBotpose();
+                    telemetry.addData("tx", result.getTx());
+                    telemetry.addData("ty", result.getTy());
+                    telemetry.addData("Botpose", botpose.toString());
+                }
+            }
+        }
+    }
+}
