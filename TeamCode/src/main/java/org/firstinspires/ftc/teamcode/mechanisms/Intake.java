b1c2445e128bcf5f026f32e698a711456c89aab5
blueseaturtle08 <113743479+blueseaturtle08@users.noreply.github.com>
2024-09-25 20:58:12 -0700

    rewritten auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Intake.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+public class Intake {
+
+//    DcMotor
+    public Intake(HardwareMap HWMap){
+
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Slides.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Slides {
+
+    public Slides(HardwareMap HWMap){
+
+    }
+}
