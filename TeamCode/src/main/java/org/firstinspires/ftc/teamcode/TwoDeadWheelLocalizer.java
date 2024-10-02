d34e8d87196eca69692ef8e2fba3dc73a2d898ee
vik-j <114836259+vik-j@users.noreply.github.com>
2024-10-01 19:02:39 -0700

    Update TwoDeadWheelLocalizer.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TwoDeadWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TwoDeadWheelLocalizer.java
@@ -52,6 +52,7 @@ public final class TwoDeadWheelLocalizer implements Localizer {
         // TODO: reverse encoder directions if needed
         //   par.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        par.setDirection(DcMotorSimple.Direction.REVERSE);
         perp.setDirection(DcMotorSimple.Direction.REVERSE);
 
         this.imu = imu;
