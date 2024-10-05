7c717646f07816a1a510e1d937e14cf1f2c484cd
Siddharth Ray <sidraystudios@gmail.com>
2024-10-05 16:01:02 -0700

    broken kalman

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/control/KalmanFilter.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.util.control;
+
+import com.arcrobotics.ftclib.geometry.Pose2d;
+
+public class KalmanFilter {
+    double kalmanGainX = 1;
+    double kalmanGainY = 1;
+    double variance
+
+    public void filter(Pose2d measurement) {
+
+    }
+}
