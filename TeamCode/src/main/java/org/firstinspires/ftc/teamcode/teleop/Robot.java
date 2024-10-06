8cac826543ef2ca30da3d5a1225ea928c00e1828
vik-j <114836259+vik-j@users.noreply.github.com>
2024-10-06 15:22:58 -0700

    configured Kotlin
    cofigured Kotlin

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Robot.java
@@ -16,12 +16,12 @@ public class Robot {
     public Robot(HardwareMap hardwareMap) {
         drive = new MecanumDrive(hardwareMap, new Pose2d(0,0,0));
 
-        flip = hardwareMap.get(DcMotor.class, "flip");
-        slide = hardwareMap.get(DcMotor.class, "slide");
-        intake = hardwareMap.get(DcMotor.class, "intake");
-        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
-        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
-        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        flip = hardwareMap.dcMotor.get("flip");
+        slide = hardwareMap.dcMotor.get("slide");
+        intake = hardwareMap.dcMotor.get("intake");
+        leftFront = hardwareMap.dcMotor.get("leftFront");
+        leftBack = hardwareMap.dcMotor.get("leftBack");
+        rightFront = hardwareMap.dcMotor.get("rightFront");
         rightBack = hardwareMap.get(DcMotor.class, "rightBack");
         claw = hardwareMap.get(Servo.class, "claw");
         wrist = hardwareMap.get(Servo.class, "wrist");
