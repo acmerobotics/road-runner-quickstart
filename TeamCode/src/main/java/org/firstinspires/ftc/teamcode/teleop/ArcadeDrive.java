a79b32cc1b3587502fd9d6b4d4272c8cf9f027be
MaximeSobrier <142960631+MaximeSobrier@users.noreply.github.com>
2024-10-06 14:48:02 -0700

    n
    h

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ArcadeDrive.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp
+public class ArcadeDrive extends LinearOpMode {
+
+    public void runOpMode() throws InterruptedException {
+        DcMotor leftFront = hardwareMap.dcMotor.get("leftFront");
+        DcMotor leftBack = hardwareMap.dcMotor.get("leftBack");
+        DcMotor rightBack = hardwareMap.dcMotor.get("rightBack");
+        DcMotor rightFront = hardwareMap.dcMotor.get("rightFront");
+
+
+        leftFront.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x * 1.1;
+            double rx = gamepad1.right_stick_x;
+
+
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double leftFrontPower = (y + x + rx) / denominator;
+            double leftBackPower = (y - x + rx) / denominator;
+            double rightFrontPower = (y - x - rx) / denominator;
+            double rightBackPower = (y + x - rx) / denominator;
+
+            leftFront.setPower(leftFrontPower);
+            leftBack.setPower(leftBackPower);
+            rightFront.setPower(rightFrontPower);
+            rightBack.setPower(rightBackPower);
+        }
+    }
+}
