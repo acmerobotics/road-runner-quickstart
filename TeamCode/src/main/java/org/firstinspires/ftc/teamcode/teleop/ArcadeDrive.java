69bd8e617fe0ac5cac2e797cfbfb20e769cd6c2b
MaximeSobrier <142960631+MaximeSobrier@users.noreply.github.com>
2024-10-06 15:08:53 -0700

    Update ArcadeDrive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ArcadeDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ArcadeDrive.java
@@ -25,7 +25,7 @@ public class ArcadeDrive extends LinearOpMode {
 
         while (opModeIsActive()) {
             double y = -gamepad1.left_stick_y;
-            double x = gamepad1.left_stick_x * 1.1;
+            double x = gamepad1.left_stick_x;
             double rx = gamepad1.right_stick_x;
 
 
