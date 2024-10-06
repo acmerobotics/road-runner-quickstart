89c76c7f353050c753ea894a90a3691e6fe3035f
vik-j <114836259+vik-j@users.noreply.github.com>
2024-10-06 15:12:27 -0700

    Update Robot.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Robot.java
@@ -8,11 +8,22 @@ import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 
 public class Robot {
+    DcMotor leftFront, leftBack, rightFront, rightBack;
     DcMotor flip, slide, intake;
     Servo claw, wrist;
     MecanumDrive drive;
+
     public Robot(HardwareMap hardwareMap) {
         drive = new MecanumDrive(hardwareMap, new Pose2d(0,0,0));
+
         flip = hardwareMap.get(DcMotor.class, "flip");
+        slide = hardwareMap.get(DcMotor.class, "slide");
+        intake = hardwareMap.get(DcMotor.class, "intake");
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+        claw = hardwareMap.get(Servo.class, "claw");
+        wrist = hardwareMap.get(Servo.class, "wrist");
     }
 }
