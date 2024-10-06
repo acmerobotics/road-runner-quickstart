8947b90611b05e7ab60b0ecf613b9ff6430bfe96
vik-j <114836259+vik-j@users.noreply.github.com>
2024-10-06 15:00:23 -0700

    Create Robot.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Robot {
+    DcMotor flip, slide, intake;
+    Servo claw, wrist;
+    MecanumDrive drive;
+    public Robot(HardwareMap hardwareMap) {
+
+    }
+}
