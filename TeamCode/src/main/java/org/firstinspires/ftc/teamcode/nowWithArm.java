fb3a418f01d3a8f5f72bb1377ba18b483860cad6
max <maxwellcosta2009@gmail.com>
2024-10-07 16:40:34 -0400

    Create nowWithArm.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nowWithArm.java
@@ -0,0 +1,108 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.robotcore.external.JavaUtil;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+
+@TeleOp(name = "ChassisMovement2425")
+public class ChassisMovement2425 extends LinearOpMode {
+
+    private IMU imu;
+    private DcMotor backRight;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor frontLeft;
+    private DcMotor arm;
+
+
+
+    private void setupMovement() {
+        imu.initialize(new IMU.Parameters(new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP, RevHubOrientationOnRobot.UsbFacingDirection.FORWARD)));
+        imu.resetYaw();
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        arm.setMode(DcMotor.RunMode.RUN_WITH_ENCODER);
+    }
+
+    @Override
+    public void runOpMode() {
+        imu = hardwareMap.get(IMU.class, "imu");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+
+        setupMovement();
+        waitForStart();
+        if (isStopRequested()) {
+            // return???
+        }
+        if (opModeIsActive()) {
+            while (opModeIsActive()) {
+                manualMove();
+            }
+        }
+    }
+
+
+
+    private void manualMove() {
+        float y;
+        float x;
+        float t;
+        double botHeading;
+        double rotX;
+        double rotY;
+        double denominator;
+        double frontLeftPower;
+        double backLeftPower;
+        double frontRightPower;
+        double backRightPower;
+        //feemfeemfeemfeem
+        y = gamepad1.left_stick_y;
+        x = -gamepad1.left_stick_x;
+        t = -gamepad1.right_stick_x;
+        armUp = gamepad1.dpad_up;
+        armDown = gamepad1.dpad_down;
+
+
+        if (gamepad1.start) {
+            imu.resetYaw();
+        }
+        botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
+        rotX = x * Math.cos(-botHeading / 180 * Math.PI) - y * Math.sin(-botHeading / 180 * Math.PI);
+        rotY = x * Math.sin(-botHeading / 180 * Math.PI) + y * Math.cos(-botHeading / 180 * Math.PI);
+        rotX = rotX * 1.1;
+        denominator = JavaUtil.maxOfList(JavaUtil.createListWith(Math.abs(rotY) + Math.abs(rotX) + Math.abs(t), 1));
+        frontLeftPower = (rotY + rotX + t) / denominator;
+        backLeftPower = (rotY - (rotX - t)) / denominator;
+        frontRightPower = (rotY - (rotX + t)) / denominator;
+        backRightPower = (rotY + (rotX - t)) / denominator;
+        frontLeft.setPower(0.75 * frontLeftPower);
+        backLeft.setPower(0.75 * backLeftPower);
+        frontRight.setPower(0.75 * frontRightPower);
+        backRight.setPower(0.75 * backRightPower);
+
+        if (armUp) {
+            arm.setPower(0.5);
+        }else if (armDown){
+            arm.setPower(-0.5);
+        }
+
+    }
+}
