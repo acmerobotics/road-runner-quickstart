f6a1b89e85e4de7e202d76dab8072c0ca2618dcc
vik-j <114836259+vik-j@users.noreply.github.com>
2024-10-07 18:34:10 -0700

    Update Robot.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Robot.java
@@ -14,20 +14,22 @@ import org.firstinspires.ftc.teamcode.MecanumDrive;
 
 public class Robot {
     DcMotor leftFront, leftBack, rightFront, rightBack;
-    DcMotor flip, slide, intake;
-    Servo claw, wrist;
+    DcMotor flip, slide;
+    Servo claw, wrist, intakeLeft, intakeRight;
     MecanumDrive drive;
 
     public Robot(HardwareMap hardwareMap) {
         drive = new MecanumDrive(hardwareMap, new Pose2d(0,0,0));
 
-        flip = hardwareMap.dcMotor.get("flip");
-        slide = hardwareMap.dcMotor.get("slide");
-        intake = hardwareMap.dcMotor.get("intake");
         leftFront = hardwareMap.dcMotor.get("leftFront");
         leftBack = hardwareMap.dcMotor.get("leftBack");
         rightFront = hardwareMap.dcMotor.get("rightFront");
         rightBack = hardwareMap.dcMotor.get("rightBack");
+
+        flip = hardwareMap.dcMotor.get("flip");
+        slide = hardwareMap.dcMotor.get("slide");
+        intakeLeft = hardwareMap.servo.get("intakeLeft");
+        intakeRight = hardwareMap.servo.get("intakeRight");
         claw = hardwareMap.servo.get("claw");
         wrist = hardwareMap.servo.get("wrist");
 
