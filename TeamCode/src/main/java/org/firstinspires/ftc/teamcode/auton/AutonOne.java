261a2372270b662eabbb1e3384eaedd73b3e2fcc
vedikavenugopal <155735931+vedikavenugopal@users.noreply.github.com>
2024-10-08 19:10:14 -0700

    .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/AutonOne.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/AutonOne.java
@@ -27,7 +27,7 @@ public class AutonOne extends LinearOpMode {
         TrajectoryActionBuilder tab1 = drive.actionBuilder(new Pose2d(0,0,0))
                 // Move to a new pose 24 inches right and 12 inches forward
                 .strafeTo(new Vector2d(24, 12)) // New position (x + 24, y + 12)
-                // Move back 10 inches (y - 10)
+                // Move back 10 inches (y - 10
                 .strafeTo(new Vector2d(24, 2))
                 // Strafe left 30 inches (x - 30)
                 .strafeTo(new Vector2d(-6, 2));
