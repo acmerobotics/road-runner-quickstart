1c667c5cef7122d7314e93fc318a2c51bbbaf6df
vik-j <114836259+vik-j@users.noreply.github.com>
2024-10-09 12:35:49 -0700

    more meepmep

--- meepmep/src/main/java/com/example/meepmep/meepmep.java
+++ meepmep/src/main/java/com/example/meepmep/meepmep.java
@@ -3,6 +3,8 @@ package com.example.meepmep;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueLight;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
@@ -12,6 +14,10 @@ public class meepmep {
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        RoadRunnerBotEntity bot2 = new DefaultBotBuilder(meepMeep)
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
@@ -30,10 +36,40 @@ public class meepmep {
                 .waitSeconds(2)
                 .build());
 
+        bot2.runAction(bot2.getDrive().actionBuilder(new Pose2d(-15, 63, Math.toRadians(270)))
+                .strafeTo(new Vector2d(-11,58))
+                .waitSeconds(2.5)
+                .strafeToLinearHeading(new Vector2d(-56,48), Math.toRadians(295))
+                .waitSeconds(3)
+                .turn(Math.toRadians(-30))
+                .waitSeconds(1)
+                .turn(Math.toRadians(30))
+                .waitSeconds(2)
+                .turn(Math.toRadians(-55))
+                .waitSeconds(1)
+                .turn(Math.toRadians(55))
+                .waitSeconds(2)
+                .turn(Math.toRadians(-25))
+                .waitSeconds(2)
+                .strafeTo(new Vector2d(-56,48))
+                .waitSeconds(2)
+                .strafeTo(new Vector2d(-11,58))
+                .waitSeconds(2)
+                .strafeTo(new Vector2d(-56,48))
+                .waitSeconds(2)
+                .strafeTo(new Vector2d(-11,58))
+                .waitSeconds(2)
+                .strafeTo(new Vector2d(-56,48))
+                .waitSeconds(2)
+                .strafeTo(new Vector2d(-11,58))
+                .waitSeconds(2)
+                .build());
+
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
+                .addEntity(bot2)
                 .start();
     }
 }
