902f5dfb81fdc0d0f0477ad76873a83fa54ea74a
masonc8419 <masonc3000@gmail.com>
2024-10-15 21:21:13 -0700

    pain and suffering

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Paths/PurpleAutoPaths.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Paths/PurpleAutoPaths.java
@@ -2,56 +2,46 @@ package org.firstinspires.ftc.teamcode.Auto.Paths;
 
 
 
-import androidx.annotation.NonNull;
-
 // RR-specific imports
-import com.acmerobotics.dashboard.config.Config;
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
-
-// Non-RR imports
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
+
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 
 @Autonomous
-public class RedAutoPaths extends LinearOpMode {
+public class PurpleAutoPaths extends LinearOpMode {
 
     @Override
     public void runOpMode() {
         Pose2d StartPose1 = new Pose2d(0, 8, 0);
         MecanumDrive drive = new MecanumDrive(hardwareMap,StartPose1);
 
-        //Pose2d StartPose1 = new Pose2d(-40, -60, 0);
-        //drive.setPoseEstimate(StartPose1);
 
-        TrajectoryActionBuilder basket = drive.actionBuilder(StartPose1)
+        TrajectoryActionBuilder basket2 = drive.actionBuilder(StartPose1)
                 .strafeToLinearHeading(new Vector2d(25,-29.5), Math.toRadians(0))
                 .waitSeconds(1)
-                .strafeToLinearHeading(new Vector2d(15.17, 18.10), Math.toRadians(90))
+                .strafeToLinearHeading(new Vector2d(15, -29.5), Math.toRadians(-180))
                 .waitSeconds(1)
                 .strafeToLinearHeading(new Vector2d(23.24,-46.10), Math.toRadians(-10))
                 .waitSeconds(1)
-                .strafeToLinearHeading(new Vector2d(15.17, 18.10), Math.toRadians(90))
+                .strafeToLinearHeading(new Vector2d(15, -46.10), Math.toRadians(-180))
                 .waitSeconds(1)
-                .strafeToLinearHeading(new Vector2d(15.17, -24), Math.toRadians(0))
                 .strafeToLinearHeading(new Vector2d(40.54, -30), Math.toRadians(-90))
                 .waitSeconds(1)
-                .strafeToLinearHeading(new Vector2d(15.17, 18.10), Math.toRadians(90))
+                .strafeToLinearHeading(new Vector2d(40.54, -30), Math.toRadians(-180))
                 .waitSeconds(1)
                 .strafeTo(new Vector2d(15,-15));
 
-        Action path = basket.build();
-                waitForStart();
-                Actions.runBlocking(path);
+        Action path2 = basket2.build();
+        waitForStart();
+        Actions.runBlocking(path2);
 
     }
 }
+
