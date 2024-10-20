b73adf3e7e41820507bdb43bc0703cffe90b12a9
vik-j <114836259+vik-j@users.noreply.github.com>
2024-10-20 16:21:29 -0700

    added stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/LimelightAngleCorrection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/LimelightAngleCorrection.java
@@ -1,11 +1,39 @@
 package org.firstinspires.ftc.teamcode.auton;
 
+import com.acmerobotics.roadrunner.Pose2d;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
 public class LimelightAngleCorrection extends LinearOpMode {
+    Limelight3A limelight;
+
     @Override
     public void runOpMode() throws InterruptedException {
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0,0,0));
 
+
+        telemetry.setMsTransmissionInterval(11);
+
+        limelight.pipelineSwitch(0);
+
+        waitForStart();
+        limelight.start();
+
+        LLResult result = limelight.getLatestResult();
+        double tx = result.getTx();
+        double ty = result.getTy();
+
+        telemetry.addData("tx", tx);
+        telemetry.addData("ty", ty);
+        telemetry.update();
+
+        if (tx >= 0) {
+
+        }
     }
 }
