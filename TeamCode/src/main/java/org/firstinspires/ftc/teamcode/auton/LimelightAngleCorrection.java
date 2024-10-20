791515993a1d129822c6e5ef3ff72df3deb6ff2f
vik-j <114836259+vik-j@users.noreply.github.com>
2024-10-20 16:29:47 -0700

    Update LimelightAngleCorrection.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/LimelightAngleCorrection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/LimelightAngleCorrection.java
@@ -1,14 +1,18 @@
 package org.firstinspires.ftc.teamcode.auton;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 
+@Autonomous
 public class LimelightAngleCorrection extends LinearOpMode {
     Limelight3A limelight;
 
@@ -19,6 +23,7 @@ public class LimelightAngleCorrection extends LinearOpMode {
         MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0,0,0));
 
         telemetry.setMsTransmissionInterval(11);
+        telemetry = new MultipleTelemetry(FtcDashboard.getInstance().getTelemetry(), telemetry);
 
         limelight.pipelineSwitch(0);
 
