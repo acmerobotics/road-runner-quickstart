88869f5b7c731eedfcf7677ffc56c9738714bf27
vik-j <114836259+vik-j@users.noreply.github.com>
2024-10-20 16:14:23 -0700

    Create LimelightAngleCorrection.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/LimelightAngleCorrection.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.auton;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class LimelightAngleCorrection extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
