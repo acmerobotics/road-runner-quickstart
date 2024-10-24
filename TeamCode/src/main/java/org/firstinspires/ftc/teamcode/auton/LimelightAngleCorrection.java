b85bf80aa9c776cc41c1e5962ba31e771d99003d
MaximeSobrier <142960631+MaximeSobrier@users.noreply.github.com>
2024-10-23 20:43:41 -0700

    Update LimelightAngleCorrection.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/LimelightAngleCorrection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/LimelightAngleCorrection.java
@@ -8,6 +8,7 @@ import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.LLResultTypes.DetectorResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.hardware.limelightvision.LLStatus;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
