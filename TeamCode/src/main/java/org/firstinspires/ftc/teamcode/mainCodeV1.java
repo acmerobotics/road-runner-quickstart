a6bfcf42eec627de7186fb47b5a47565e014e2c6
FunaCat <75957785+FunaCat@users.noreply.github.com>
2024-10-25 14:24:19 -0400

    creation of mainCodeV1
    main code version control doc is in google drive.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainCodeV1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainCodeV1.java
@@ -1,21 +1,33 @@
 package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
 
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.IMU;
-import com.qualcomm.robotcore.hardware.ColorSensor;
-
 
 import org.firstinspires.ftc.robotcore.external.JavaUtil;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
 
-import java.lang.Math;
 
 
-@TeleOp(name = "nowWithArm")
-public class nowWithArm extends LinearOpMode {
+
+@TeleOp(name = "mainCodeV1")
+public class mainCodeV1 extends LinearOpMode {
+    // variable declaration
     private IMU imu;
     private DcMotor backRight;
     private DcMotor frontRight;
@@ -23,17 +35,24 @@ public class nowWithArm extends LinearOpMode {
     private DcMotor frontLeft;
     private DcMotor arm;
     private ColorSensor colorDetector;
+    int ARMMIN;
+    int ARMMAX;
+    int INCREMENT;
 
-    private void setupMovement() {
+
+    private void setupChassis() {
         imu.initialize(new IMU.Parameters(new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP, RevHubOrientationOnRobot.UsbFacingDirection.FORWARD)));
         imu.resetYaw();
+
         backRight.setDirection(DcMotor.Direction.REVERSE);
         frontRight.setDirection(DcMotor.Direction.REVERSE);
+
         backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
         backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -70,6 +89,7 @@ public class nowWithArm extends LinearOpMode {
         frontRight.setPower(0.75 * frontRightPower);
         backRight.setPower(0.75 * backRightPower);
     }
+
     private void armMovement(int ARMMAX, int ARMMIN, int INCREMENT) {
         int armPosition = arm.getCurrentPosition();
         if (gamepad1.dpad_down) {       // if (DPAD-down) is being pressed and if not yet the min
@@ -81,11 +101,9 @@ public class nowWithArm extends LinearOpMode {
         arm.setTargetPosition(armPosition);
     }
 
-    private void printPosition(int ARMMAX, int ARMMIN) {
+    private void printPosition() {
         int position = arm.getCurrentPosition();
         int target = arm.getTargetPosition();
-        telemetry.addData("max: ", ARMMAX);
-        telemetry.addData("min: ", ARMMIN);
 
         telemetry.addData("Encoder position, ", position);
         telemetry.addData("Encoder target, ", target);
@@ -96,8 +114,7 @@ public class nowWithArm extends LinearOpMode {
         telemetry.update();
     }
 
-    @Override
-    public void runOpMode() {
+    private void hardwareMapping() {
         imu = hardwareMap.get(IMU.class, "imu");
         backRight = hardwareMap.get(DcMotor.class, "backRight");
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
@@ -105,21 +122,36 @@ public class nowWithArm extends LinearOpMode {
         frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
         arm = hardwareMap.get(DcMotor.class, "arm");
         colorDetector = hardwareMap.get(ColorSensor.class, "colorDetector");
-        setupMovement();
+    }
 
-        // Arm SetUp
-        final int ARMMIN = arm.getCurrentPosition() - 3;
-        final int ARMMAX = ARMMIN - 3200;
-        final int INCREMENT = 250;
+    private void initializeSetUp() {
+        hardwareMapping();
+        setupChassis();
+        armSetup();
+    }
+
+    private void armSetup() {
         arm.setPower(1);
-        waitForStart();
+        ARMMIN = arm.getCurrentPosition() - 3;
+        ARMMAX = ARMMIN - 3200;
+        INCREMENT = 250;
+    }
 
+    private void startSetUp() {
         arm.setTargetPosition(arm.getCurrentPosition());
         arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+
+
+    @Override
+    public void runOpMode() {
+        initializeSetUp();
+        waitForStart();
+        startSetUp();
         while (opModeIsActive()) {
             chassisMovement();
             armMovement(ARMMAX, ARMMIN, INCREMENT);
-            printPosition(ARMMAX, ARMMIN);
+            printPosition();
         }
     }
 }
