9f7ccc39b4d754cb2293b02f3133727ba294bc0b
Nikhil201212 <Nikster725@gmail.com>
2024-10-30 18:29:46 -0700

    more tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -64,13 +64,13 @@ public final class MecanumDrive {
 
         // drive model parameters
         public double inPerTick = 0.0029502876530462;
-        public double lateralInPerTick = inPerTick; // we have not defined this yet
-        public double trackWidthTicks = 4508.614072504213;
+        public double lateralInPerTick = 0.0029502876530462;
+        public double trackWidthTicks = 4264.681383859044   ;
 
         // feedforward parameters (in tick units)
         public double kS = 0.8882966267235468;
         public double kV = 0.0005948339515810528;
-        public double kA = 0;
+        public double kA = 0.00001;
 
         // path profile parameters (in inches)
         public double maxWheelVel = 50;
@@ -82,9 +82,9 @@ public final class MecanumDrive {
         public double maxAngAccel = Math.PI;
 
         // path controller gains
-        public double axialGain = 0.0;
-        public double lateralGain = 0.0;
-        public double headingGain = 0.0; // shared with turn
+        public double axialGain = 6;
+        public double lateralGain =6;
+        public double headingGain = 4; // shared with turn
 
         public double axialVelGain = 0.0;
         public double lateralVelGain = 0.0;
