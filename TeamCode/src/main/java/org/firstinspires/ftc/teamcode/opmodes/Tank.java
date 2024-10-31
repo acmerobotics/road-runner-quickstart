424afe12e7d82a15592f786ebadaf38722f78318
Jonathan Sutjandra <zonzeluc2@gmail.com>
2024-10-31 17:29:36 -0500

    deleted some comments and made the code easier to read

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Tank.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Tank.java
@@ -25,32 +25,12 @@ public class Tank extends OpMode {
     DcMotorEx rightFront;
 
     Shoulder shoulder = new Shoulder(this);
-    // Assuming you're using StandardTrackingWheelLocalizer.java
-    // Switch this class to something else (Like TwoWheeTrackingLocalizer.java) if your configuration is different
+
     MecanumDrive drive;
-    // Set your initial pose to x: 10, y: 10, facing 90 degrees
-    // Make sure to call drive.update() on *every* loop
-    // Increasing loop time by utilizing bulk reads and minimizing writes will increase your odometry accuracy
 
-//        // Retrieve your pose
-//        Pose2d cur = drive.pose;
-//
-//        Telemetry.;
-//        telemetry.addData("y", myPose.getY());
-//        telemetry.addData("heading", myPose.getHeading());
-
-    // Insert whatever teleop code you're using
-
-    // Read pose
     Pose2d poseEstimate;
     Vector2d input;
 
-    // Create a vector from the gamepad x/y inputs
-// Then, rotate that vector by the inverse of that heading
-
-
-// Pass in the rotated input + right stick value for rotation
-// Rotation is not part of the rotated input thus must be passed in separately
 
 
 
@@ -77,10 +57,11 @@ public class Tank extends OpMode {
         leftBack.setPower(Lx * max);
         rightFront.setPower(Rx * max);
         rightBack.setPower(Rx * max);
+
+
         if (gamepad1.left_trigger != 0 && gamepad1.right_trigger != 0) {
             ;
-        }
-        else if (gamepad1.left_trigger != 0) {
+        } else if (gamepad1.left_trigger != 0) {
             leftFront.setPower(-max * gamepad1.left_trigger);
             leftBack.setPower(max * gamepad1.left_trigger);
             rightFront.setPower(max * gamepad1.left_trigger);
