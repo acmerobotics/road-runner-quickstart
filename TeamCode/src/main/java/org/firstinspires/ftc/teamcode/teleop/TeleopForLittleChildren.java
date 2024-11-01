f8750a182e48ab6a396cc2c7a305ca412d734916
vik-j <114836259+vik-j@users.noreply.github.com>
2024-11-01 16:25:23 -0700

    Update TeleopForLittleChildren.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleopForLittleChildren.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleopForLittleChildren.java
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+@TeleOp
 public class TeleopForLittleChildren extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
