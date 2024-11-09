c49fa6b4288ea2ab1bfaa27f40af0d346e8f8281
Jonathan Sutjandra <zonzeluc2@gmail.com>
2024-11-09 10:26:45 -0600

    made meepmeepjonathan, my testing site for meepmeep

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/meepmeepjonathan.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/meepmeepjonathan.java
@@ -6,7 +6,7 @@ import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-public class MeepMeepTesting {
+public class meepmeepjonathan {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
@@ -15,9 +15,23 @@ public class MeepMeepTesting {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(13, 35, Math.toRadians(0)))
-                .lineToX(34)
-                .strafeTo(new Vector2d(36, 26))
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(14, -61, Math.toRadians(90)))
+                .strafeTo(new Vector2d(10, -34))
+                //put arm up while strafing
+                //stop at (10, -34) and place the sample on the bar
+                .setReversed(true)
+                .splineTo(new Vector2d(30, -36), Math.toRadians(0))
+                //move arm down to gathering position while splining
+                .splineTo(new Vector2d(35, -5), Math.toRadians(90))
+                .setReversed(false)
+
+                .splineToConstantHeading(new Vector2d(48, -20), Math.toRadians(-90))
+
+                .splineToConstantHeading(new Vector2d(48, -50), Math.toRadians(-90))
+
+
+                //run intake while strafing to point
+                //end intake after meeting the point
 
                 .build());
 
