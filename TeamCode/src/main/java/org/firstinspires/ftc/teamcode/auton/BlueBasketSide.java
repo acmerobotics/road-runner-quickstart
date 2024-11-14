2761fa8d305f1c80ba499a9d4843a5bc921d5f53
MaximeSobrier <142960631+MaximeSobrier@users.noreply.github.com>
2024-11-13 18:42:49 -0800

    a

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/BlueBasketSide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/BlueBasketSide.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.auton;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.ParallelAction;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
@@ -9,8 +10,10 @@ import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.teamcode.teleop.Robot;
 
 @Config
 @Autonomous(name = "BlueBasketSide", group = "Autonomous")
@@ -20,8 +23,10 @@ public class BlueBasketSide extends LinearOpMode {
         // Starting position of the robot (x = -11.8, y = -61.7, heading = -90 degrees)
         Pose2d initialPose = new Pose2d(15, 63, Math.toRadians(270));
         MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+        Robot bot = new Robot(hardwareMap);
 
-        
+
+
         // Define trajectory using Pose2d for simultaneous right and forward movement
         TrajectoryActionBuilder tab1 = drive.actionBuilder(initialPose)
                 .strafeTo(new Vector2d(9,48))
@@ -59,17 +64,34 @@ public class BlueBasketSide extends LinearOpMode {
         // Final action to close out the trajectory
         Action trajectoryActionCloseOut = tab1.fresh().build();
 
+        Action waitAndArm = drive.actionBuilder(initialPose)
+                .afterTime(0,bot.setPidVals(600,3600, 0.3))
+                .afterTime(5, bot.setPidVals(0,3600,0.3))
+                .build();
+
+        bot.slide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bot.flip.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        bot.slide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        bot.flip.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
         // Wait for the start of the op mode
         waitForStart();
+        bot.setPidValues(0,0,0.5);
         if (isStopRequested()) return;
+        Robot.stopPid = false;
 
         // Execute the defined trajectory
         Action trajectoryActionChosen = tab1.build();
         Actions.runBlocking(
-                new SequentialAction(
-                        trajectoryActionChosen,
-                        trajectoryActionCloseOut
+                new ParallelAction(
+                        new SequentialAction(
+                                trajectoryActionChosen,
+                                trajectoryActionCloseOut),
+                        waitAndArm,
+                        bot.getPIDAction()
                 )
         );
+        bot.stopPidAction();
     }
 }
