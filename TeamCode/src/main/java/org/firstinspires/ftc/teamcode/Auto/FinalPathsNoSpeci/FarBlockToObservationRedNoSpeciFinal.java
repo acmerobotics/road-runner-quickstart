dbff10048d1114ad1978a45d25858bdd628edac7
anvikiran <134178652+anvikiran@users.noreply.github.com>
2024-11-15 19:02:46 -0800

    11/15

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/FinalPathsNoSpeci/FarBlockToObservationRedNoSpeciFinal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/FinalPathsNoSpeci/FarBlockToObservationRedNoSpeciFinal.java
@@ -36,97 +36,27 @@ public class FarBlockToObservationRedNoSpeciFinal extends LinearOpMode {
         //Pose2d StartPose1 = new Pose2d(-40, -60, 0);
         //drive.setPoseEstimate(StartPose1);
 
-        TrajectoryActionBuilder model = drive.actionBuilder(StartPose1)
-                //.strafeToLinearHeading(new Vector2d(-29.46,-12.24), Math.toRadians(0))
-                //.waitSeconds(1)
-                .strafeToLinearHeading(new Vector2d(-9.42,41.09), Math.toRadians(180))
-                .waitSeconds(1)
-                .strafeToLinearHeading(new Vector2d(-12.55,37.21), Math.toRadians(0))
-                .waitSeconds(1)
-                .strafeToLinearHeading(new Vector2d(-9.86,49.11), Math.toRadians(180))
-                .waitSeconds(1)
-                .strafeToLinearHeading(new Vector2d(-14.55,37.21), Math.toRadians(0))
-                .waitSeconds(1)
-                .strafeToLinearHeading(new Vector2d(-28.81,38.9), Math.toRadians(90))
-                .waitSeconds(1)
-                .strafeToLinearHeading(new Vector2d(-14.55,37.21), Math.toRadians(0))
-                .waitSeconds(4)
-                .strafeToLinearHeading(new Vector2d(4.85,40), Math.toRadians(180));
+        TrajectoryActionBuilder basketStartTraj = drive.actionBuilder(drive.pose)
+                .strafeToLinearHeading(new Vector2d(-21.66, 9.5), Math.toRadians(45));
+        TrajectoryActionBuilder park = drive.actionBuilder(drive.pose)
+                .strafeToLinearHeading(new Vector2d(0, 0), Math.toRadians(0));
 
-        TrajectoryActionBuilder upALil = drive.actionBuilder(StartPose1)
-                .strafeToConstantHeading(new Vector2d(0, 12))
-                .waitSeconds(1);
-        TrajectoryActionBuilder basket = drive.actionBuilder(StartPose1)
-                .strafeToLinearHeading(new Vector2d(-69.66, 9.5), Math.toRadians(45))
-                .waitSeconds(1);
-        TrajectoryActionBuilder observation = drive.actionBuilder(StartPose1)
-                .strafeToConstantHeading(new Vector2d(30,9.5))
-                .waitSeconds(1);
-        TrajectoryActionBuilder firstBlock = drive.actionBuilder(StartPose1)
-                .strafeToLinearHeading(new Vector2d(-12.55,37.21), Math.toRadians(0))
-                .waitSeconds(1);
-        TrajectoryActionBuilder secondBlock = drive.actionBuilder(StartPose1)
-                .strafeToLinearHeading(new Vector2d(-14.55,37.21), Math.toRadians(0))
-                .waitSeconds(1);
-        TrajectoryActionBuilder thirdBlock = drive.actionBuilder(StartPose1)
-                .strafeToLinearHeading(new Vector2d(-28.81,38.9), Math.toRadians(90))
-                .waitSeconds(1);
-        TrajectoryActionBuilder parking = drive.actionBuilder(StartPose1)
-                .strafeToLinearHeading(new Vector2d(-14.55,37.21), Math.toRadians(0))
-                .waitSeconds(4);
-
-        Action up = upALil.build();
-        Action toBasket = basket.build();
-        Action toObservation = observation.build();
-        Action block1 = firstBlock.build();
-        Action block2 = secondBlock.build();
-        Action block3 = thirdBlock.build();
-        Action park = parking.build();
+        Action basket1 = basketStartTraj.build();
+        Action parking = park.build();
 
         waitForStart();
         Actions.runBlocking(new SequentialAction(
-                intake.flop(),
-                up,
-                toBasket,
+                claw.flop(),
+                basket1,
                 slides.slideTopBasket(),
                 claw.flip(),
                 new SleepAction(0.5),
                 claw.flop(),
-                new SleepAction(1),
                 slides.retract(),
-                new SleepAction(1),
-                toObservation
+                parking
 
-                /*
-                intake.flip(),
-                intake.extake(),
-                intake.flop(),
-                block1,
-                intake.flip(),
-                intake.intake(),
-                intake.flop(),
-                toObservation,
-                intake.flip(),
-                intake.extake(),
-                intake.flop(),
-                block2,
-                intake.flip(),
-                intake.intake(),
-                intake.flop(),
-                toObservation,
-                intake.flip(),
-                intake.extake(),
-                intake.flop(),
-                block3,
-                intake.flip(),
-                intake.intake(),
-                intake.flop(),
-                toObservation,
-                intake.flip(),
-                intake.extake(),
-                intake.flop(),
-                park
-                */
+
+
 
 
 
