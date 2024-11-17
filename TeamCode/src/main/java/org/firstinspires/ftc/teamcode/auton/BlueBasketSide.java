ff1c211d2532a0a5ebe4b3dd20e0c14dd922840a
vik-j <114836259+vik-j@users.noreply.github.com>
2024-11-16 20:15:29 -0800

    Update BlueBasketSide.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/BlueBasketSide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/BlueBasketSide.java
@@ -68,7 +68,7 @@ public class BlueBasketSide extends LinearOpMode {
         Action trajectoryActionCloseOut = tab1.fresh().build();
 
         Action waitAndArm = drive.actionBuilder(initialPose)
-                .afterTime(0.01, bot.setPidVals(1080,4100))
+                .afterTime(0.01, bot.setPidVals(1065,4100))
 //                .afterTime(0.05, bot.intake(-0.5))
                 .afterTime(0.02, telemetryPacket -> {
                     bot.wrist.setPosition(0.01);
@@ -131,8 +131,8 @@ public class BlueBasketSide extends LinearOpMode {
                     bot.wrist.setPosition(0.07);
                     return false;
                 })
-                .afterTime(18.5, bot.setPidVals(2100,0))
-                .afterTime(20, bot.setPidVals(0,0))
+                .afterTime(17.75, bot.setPidVals(2100,0))
+                .afterTime(18.5, bot.setPidVals(0,0))
                 .afterTime(22, bot.setPidVals(0, 1800))
                 .afterTime(22.1, telemetryPacket -> {
                     bot.intakeLeft.setPower(1);
