ff289a07429eb762cb56cffab5616e0ec8163f9f
Jonathan Sutjandra <zonzeluc2@gmail.com>
2024-11-21 18:37:20 -0600

    altered right auto to align with shreyansh's changes to auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/rightAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/rightAuto.java
@@ -67,32 +67,19 @@ public class rightAuto extends LinearOpMode {
                     .waitSeconds(0.5)
                     .afterTime(0, shoulder.autonDownHC())
                     .waitSeconds(1)
-                    .setReversed(true)
-                    .strafeTo(new Vector2d(10, -36))
-                    .splineTo(new Vector2d(28, -36), Math.toRadians(0))
-                    //move arm down to gathering position while splining
-                    .afterTime(0, shoulder.autonDown())
-//                    .splineTo(new Vector2d(33, -5), Math.toRadians(90)) //successfully hooked and moved away
-
-//                    .setReversed(false)
-//
-//                    .splineToConstantHeading(new Vector2d(47, -20), Math.toRadians(-90))
-//
-//                    .splineToConstantHeading(new Vector2d(47, -50), Math.toRadians(-90))
-//                    //shrey code starts here
-//                    .setReversed(true)
-//                    .strafeTo(new Vector2d(47, -10))
-//
-//
-//                    //.splineToConstantHeading(new Vector2d(52,-45), Math.toRadians
-//                    .strafeTo(new Vector2d(56,-10))
-//
-//                    .strafeTo(new Vector2d(56,-53))
-//
-//                    .afterTime(0, viper.autonGolf())
-//                    .waitSeconds(0.3)
-//                    .setReversed(true)
-//                    .splineToLinearHeading(new Pose2d(60,-10, Math.toRadians(0)), Math.toRadians(0))
+                    .strafeTo(new Vector2d(0, 34))
+                    .waitSeconds(2)
+                    .strafeToLinearHeading(new Vector2d(0,-55), Math.toRadians(-90))
+                    .strafeTo(new Vector2d(35,-44))
+                    .strafeTo(new Vector2d(35,-5))
+                    .strafeTo(new Vector2d(45,-5))
+                    .strafeTo(new Vector2d(45,-53))
+                    .strafeTo(new Vector2d(45,-5))
+                    .strafeTo(new Vector2d(55,-5))
+                    .strafeTo(new Vector2d(55,-53))
+                    .strafeTo(new Vector2d(55,-5))
+                    .strafeTo(new Vector2d(61,-5))
+                    .strafeTo(new Vector2d(61,-53))
                     .strafeToConstantHeading(new Vector2d(60,-56))
                     //.waitSeconds(0.3)
                     //.afterTime(0, viper.autonDown())
