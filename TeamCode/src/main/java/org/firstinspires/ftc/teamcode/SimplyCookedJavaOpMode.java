35b3ef242d74e33069765166476868e7707cb05a
Sqidwarddd <nariiispsycho@gmail.com>
2024-11-27 10:02:50 -0500

    Create SimplyCookedJavaOpMode.java
    Added and declared motors

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimplyCookedJavaOpMode.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class SimplyCookedJavaOpMode extends LinearOpMode {
+    //Declare motors
+    public DcMotor FrontRight;
+    public DcMotor FrontLeft;
+    public DcMotor BackLeft;
+    public DcMotor BackRight;
+}
