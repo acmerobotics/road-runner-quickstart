eab6a56137081ed7e1c198e75023ea1749e43cdd
Henry <henry.seefeldt@ums.legacypca.org>
2024-12-03 14:55:39 -0600

    Added MeepMeep Testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Spec_Auto_Prototype.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Spec_Auto_Prototype.java
@@ -13,8 +13,8 @@ import org.firstinspires.ftc.teamcode.actions.ArmActions;
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 
 @Config
-@Autonomous(name = "Spec_Auto", group = "Autonomous")
-public class Spec_Auto extends LinearOpMode {
+@Autonomous(name = "Spec_Auto_Prototype", group = "Autonomous")
+public class Spec_Auto_Prototype extends LinearOpMode {
 
     @Override
     public void runOpMode() {
@@ -43,18 +43,7 @@ public class Spec_Auto extends LinearOpMode {
                 .waitSeconds(1)
                 .strafeTo(new Vector2d(startPose.position.x, startPose.position.y-3));
 
-        TrajectoryActionBuilder traj_3 = drive.actionBuilder(new Pose2d(startPose.position.x, startPose.position.y, Math.toRadians(90)))
-            .strafeTo(new Vector2d(-35, 38))
-            .strafeTo(new Vector2d(-35, 10))
-            .strafeToLinearHeading(new Vector2d(-45, 10), Math.toRadians(270))
-            .strafeTo(new Vector2d(-45,53))
-            .strafeTo(new Vector2d(-45, 10))
-            .strafeTo(new Vector2d(-55, 10))
-            .strafeTo(new Vector2d(-55, 53))
-            .strafeTo(new Vector2d(-35, 62))
-            .strafeToLinearHeading(new Vector2d(subPoseMid.position.x - 10, subPoseMid.position.y), Math.toRadians(90));
-
-        TrajectoryActionBuilder traj_4 = drive.actionBuilder(new Pose2d(-10, 60, Math.toRadians(90)))
+        TrajectoryActionBuilder traj_3 = drive.actionBuilder(new Pose2d(-10, 60, Math.toRadians(90)))
                 .strafeTo(new Vector2d(startPose.position.x-30, startPose.position.y));
 
 
