0d422dddc50f3cfbd9a8ac14e0b3309e97c5c221
GS3011 <gautamgoatsharma@gmail.com>
2024-12-22 16:14:19 -0800

    right4specimens

--- FtcRobotController/MeepMeep/src/main/java/com/example/meepmeep/MeepMeepTesting.java
+++ FtcRobotController/MeepMeep/src/main/java/com/example/meepmeep/MeepMeepTesting.java
@@ -9,7 +9,7 @@ import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(1000);
+        MeepMeep meepMeep = new MeepMeep(750);
 
 
 
@@ -17,22 +17,31 @@ public class MeepMeepTesting {
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(80, 80, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(-8, -61, Math.toRadians(90)))
-                        .splineToLinearHeading(new Pose2d(-57, -50, Math.toRadians(224)), Math.toRadians(180))
-                        .waitSeconds(3)
-                        .splineToLinearHeading(new Pose2d(-48, -47, Math.toRadians(224)), Math.toRadians(0))
-                        //.splineToConstantHeading(new Vector2d(-50, -45), Math.toRadians(0))
-                        .splineToLinearHeading(new Pose2d(-47, -40, Math.toRadians(90)), Math.toRadians(100))
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(8, -61, Math.toRadians(90)))
+                        .splineToLinearHeading(new Pose2d(8, -34, Math.toRadians(90)), Math.toRadians(90))
+                        .waitSeconds(2)
+                        .splineToLinearHeading(new Pose2d(8, -42, Math.toRadians(90)), Math.toRadians(0))
+                        .splineToLinearHeading(new Pose2d(48, -38, Math.toRadians(90)), Math.toRadians(0))
                         .waitSeconds(1)
-                        .splineToLinearHeading(new Pose2d(-56, -52, Math.toRadians(226)), Math.toRadians(180))
-                        .waitSeconds(3)
-                        .splineToConstantHeading(new Vector2d(-50, -45), Math.toRadians(0))
-                        .splineToLinearHeading(new Pose2d(-58, -40, Math.toRadians(90)), Math.toRadians(200))
+                        .splineToLinearHeading(new Pose2d(50, -58, Math.toRadians(90)), Math.toRadians(0))
+                        .waitSeconds(2)
+                        .splineToLinearHeading(new Pose2d(57, -38, Math.toRadians(90)), Math.toRadians(0))
                         .waitSeconds(1)
-                        .splineToLinearHeading(new Pose2d(-56, -52, Math.toRadians(226)), Math.toRadians(180))
-                        .waitSeconds(3)
-                        .splineToConstantHeading(new Vector2d(-48, -48), Math.toRadians(0))
-                        .splineToLinearHeading(new Pose2d(-30, 12, Math.toRadians(0)), Math.toRadians(70))
+                        .splineToLinearHeading(new Pose2d(50, -58, Math.toRadians(90)), Math.toRadians(0))
+                        .waitSeconds(2)
+                        .splineToLinearHeading(new Pose2d(50, -48, Math.toRadians(90)), Math.toRadians(0))
+                        .splineToLinearHeading(new Pose2d(2, -34, Math.toRadians(90)), Math.toRadians(0))
+                        .waitSeconds(1)
+                        .splineToLinearHeading(new Pose2d(8, -46, Math.toRadians(90)), Math.toRadians(0))
+                        .splineToLinearHeading(new Pose2d(50, -58, Math.toRadians(90)), Math.toRadians(0))
+                        .waitSeconds(2)
+                        .splineToLinearHeading(new Pose2d(50, -48, Math.toRadians(90)), Math.toRadians(0))
+                        .splineToLinearHeading(new Pose2d(-7, -34, Math.toRadians(90)), Math.toRadians(0))
+                        .waitSeconds(1)
+                        .splineToLinearHeading(new Pose2d(7, -48, Math.toRadians(90)), Math.toRadians(0))
+                        .splineToLinearHeading(new Pose2d(40, -48, Math.toRadians(90)), Math.toRadians(0))
+                        .splineToLinearHeading(new Pose2d(28, 10, Math.toRadians(90)), Math.toRadians(100))
+
                         .build());
 
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Right4specimens.java
@@ -0,0 +1,124 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Common.Claw;
+import org.firstinspires.ftc.teamcode.Common.Extension;
+import org.firstinspires.ftc.teamcode.Common.Fourbar;
+import org.firstinspires.ftc.teamcode.Common.Lift;
+import org.firstinspires.ftc.teamcode.Common.Rotation;
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+@Config
+@Autonomous(name = "Right4specimens", group = "Autonomous")
+public class Right4specimens extends LinearOpMode {
+    public void runOpMode() {
+        Pose2d initialPose = new Pose2d(8, -61, Math.toRadians(90));
+
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+        Claw claw = new Claw(hardwareMap);
+        Fourbar fourbar = new Fourbar(hardwareMap);
+        Rotation rotation = new Rotation(hardwareMap);
+        Extension extension = new Extension(hardwareMap);
+        Lift lift = new Lift(hardwareMap);
+
+       /* TrajectoryActionBuilder tab1 = drive.actionBuilder(initialPose)
+                .splineToConstantHeading(new Vector2d(0, -38), Math.toRadians(90));
+        Action park = tab1.endTrajectory().fresh()
+                //.lineToYConstantHeading(-38)
+                .splineToConstantHeading(new Vector2d(60,-60), Math.toRadians(180))
+                .build();*/
+
+        TrajectoryActionBuilder tab1 = drive.actionBuilder(initialPose)
+                .splineToLinearHeading(new Pose2d(8, -34, Math.toRadians(90)), Math.toRadians(90))
+                .waitSeconds(2)
+                .splineToLinearHeading(new Pose2d(8, -42, Math.toRadians(90)), Math.toRadians(0))
+                .splineToLinearHeading(new Pose2d(48, -38, Math.toRadians(90)), Math.toRadians(0))
+                .waitSeconds(1)
+                .splineToLinearHeading(new Pose2d(50, -58, Math.toRadians(90)), Math.toRadians(0))
+                .waitSeconds(2)
+                .splineToLinearHeading(new Pose2d(57, -38, Math.toRadians(90)), Math.toRadians(0))
+                .waitSeconds(1)
+                .splineToLinearHeading(new Pose2d(50, -58, Math.toRadians(90)), Math.toRadians(0))
+                .waitSeconds(2)
+                .splineToLinearHeading(new Pose2d(50, -48, Math.toRadians(90)), Math.toRadians(0))
+                .splineToLinearHeading(new Pose2d(2, -34, Math.toRadians(90)), Math.toRadians(0))
+                .waitSeconds(1)
+                .splineToLinearHeading(new Pose2d(8, -46, Math.toRadians(90)), Math.toRadians(0))
+                .splineToLinearHeading(new Pose2d(50, -58, Math.toRadians(90)), Math.toRadians(0))
+                .waitSeconds(2)
+                .splineToLinearHeading(new Pose2d(50, -48, Math.toRadians(90)), Math.toRadians(0))
+                .splineToLinearHeading(new Pose2d(-7, -34, Math.toRadians(90)), Math.toRadians(0))
+                .waitSeconds(1)
+                .splineToLinearHeading(new Pose2d(7, -48, Math.toRadians(90)), Math.toRadians(0))
+                .splineToLinearHeading(new Pose2d(40, -48, Math.toRadians(90)), Math.toRadians(0));
+
+
+        Action back = tab1.endTrajectory().fresh()
+
+                .build();
+
+
+        Action park = tab1.endTrajectory().fresh()
+                .splineToLinearHeading(new Pose2d(28, 10, Math.toRadians(90)), Math.toRadians(100))
+                .build();
+
+
+
+                //.splineToConstantHeading(new Vector2d(60,-30), Math.toRadians(180));
+
+
+
+
+        // actions that need to happen on init; for instance, a claw tightening.
+        Actions.runBlocking(claw.ClawOpen());
+        Actions.runBlocking(fourbar.FourBarUp());
+        Actions.runBlocking(rotation.RotationHorizontal());
+
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        Action toChambers = tab1.build();
+
+
+
+
+        Actions.runBlocking(
+                new SequentialAction(
+                        toChambers,
+                        back
+                        //claw.ClawClose(),
+                        //fourbar.FourBarDown(),
+                        //lift.SlidesToBar(),
+                        //new SleepAction(30)
+                )
+        );
+    }
+}
+/*
+spline 1(to chambers)
+
+linear slides up
+
+four bar set position 90 degrees
+
+coaxial set position 90 degrees
+
+linear slides down (not fully)
+
+claw open
+
+linear slides down fully
+
+spline 2 (park)
+ */
