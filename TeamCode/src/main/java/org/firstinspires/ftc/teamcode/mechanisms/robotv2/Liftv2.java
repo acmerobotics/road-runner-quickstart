62e85abc39d7194193f0706e2f821c43e1a4f3c4
asbeast <144757596+asbeast@users.noreply.github.com>
2025-01-02 23:05:49 -0800

    expanding teleop code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/robotv2/Liftv2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/robotv2/Liftv2.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.mechanisms.robotv2;
 
 
 
+
 import androidx.annotation.NonNull;
 
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
@@ -18,7 +19,6 @@ public class Liftv2 {
     final double LIFT_COLLAPSED = 0 * LIFT_TICKS_PER_MM;
     final double LIFT_SCORING_IN_LOW_BASKET = 0 * LIFT_TICKS_PER_MM;
     final double LIFT_SCORING_IN_HIGH_BASKET = 510 * LIFT_TICKS_PER_MM * 1.3;
-
 
     public DcMotorEx motor;
     double liftPosition;
@@ -43,7 +43,7 @@ public class Liftv2 {
         public boolean run(@NonNull TelemetryPacket packet) {
 
             motor.setTargetPosition(_targetPos);
-            motor.setVelocity(3000);
+            motor.setVelocity(5000);
             motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
             int currentPosition = motor.getCurrentPosition();
