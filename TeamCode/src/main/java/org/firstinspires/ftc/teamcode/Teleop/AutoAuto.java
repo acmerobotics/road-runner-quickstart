696985d35d3a9eb8650a298ffb4e7f5c945d0f3d
Daniel Yeh <somethings8596@gmail.com>
2025-01-08 23:35:29 -0800

    Completely removed heading, temporarily removed mappingTable for debugging. Hopefully works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/AutoAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/AutoAuto.java
@@ -20,19 +20,18 @@ public class AutoAuto extends LinearOpMode {
         Pose2d StartPose = new Pose2d(0,0, Math.toRadians(0));
         MecanumDrive drive = new MecanumDrive(hardwareMap, StartPose);
 
-        Gamepad currentGamepad1 = new Gamepad();
-        Gamepad currentGamepad2 = new Gamepad();
+        Gamepad currentGamepad1 = gamepad1;
+        Gamepad currentGamepad2 = gamepad2;
 
         boolean mappingState = false; // Switch to make sure no infinite execution
 
-        Map<String, Double> mappingTable = new HashMap<>();
+        // Map<String, Double> mappingTable = new HashMap<>();
 
 
         double mapping_x = 0.00; // Place holder for last X pos
         double mapping_y = 0.00; // Place holder for last Y pos
-        double mapping_heading = 0.00;
 
-        int count = 0; // Count variable for dict keys, temporary solution? Hopefully better in the future
+        int count = 0; // Count variable for dict/hashmap for data storage, temporary solution? Hopefully better in the future
 
         waitForStart();
 
@@ -40,24 +39,16 @@ public class AutoAuto extends LinearOpMode {
 
             double x = drive.pose.position.x; // drive pos x
             double y = drive.pose.position.y; // drive pos y
-            //double heading = drive.pose.heading.toDouble();
-
             if (currentGamepad1.a && !mappingState){
                 mapping_x = x;
                 mapping_y = y;
-                //mapping_heading = heading;
 
-                String x_key = String.format("x_pos_%s", count);
-                String y_key = String.format("y_pos_%s", count);
-                String heading_key = String.format("heading_%s", count);
+                String x_key = "x_pos_" + count;
+                String y_key = "y_pos_" + count;
 
-                // Implement heading after testing x and y pos
-
-                mappingTable.put(x_key, x); // Insert values into a dict with x_pos_{count} and y_pos_{count}
-                mappingTable.put(y_key, y);
-                //mappingTable.put(heading_key, heading); // Insert heading into a dict with heading_{count}
-
-                count += 1;
+                /* mappingTable.put(x_key, x); // Insert values into a dict with x_pos_{count} and y_pos_{count}
+                mappingTable.put(y_key, y); */
+                count++;
 
                 mappingState = true;
             } else{
@@ -68,11 +59,8 @@ public class AutoAuto extends LinearOpMode {
 
             telemetry.addData("X Coord", x);
             telemetry.addData("Y Coord", y);
-            //telemetry.addData("Heading", heading);
             telemetry.addData("Last X", mapping_x);
             telemetry.addData("Last Y", mapping_y);
-            telemetry.addData("Last Heading", mapping_heading);
-            telemetry.addData("Test", mappingTable); // Displays complete dict for running mapping values
             telemetry.update();
         }
     }
