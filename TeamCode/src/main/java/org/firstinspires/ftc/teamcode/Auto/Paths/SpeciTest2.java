e2fcc89243bb94bbdc3d6dbf1f1551256bbd17c7
anvikiran <134178652+anvikiran@users.noreply.github.com>
2025-01-08 20:32:47 -0800

    Create SpeciTest2.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Paths/SpeciTest2.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode.Auto.Paths;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+@Autonomous
+public class SpeciTest2 extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+
+        Pose2d StartPose1 = new Pose2d(0,0, Math.toRadians(0));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, StartPose1);
+
+
+        TrajectoryActionBuilder path = drive.actionBuilder(StartPose1)
+                .strafeToLinearHeading(new Vector2d(-8, 30), Math.toRadians(180)) //pick up 1
+                .waitSeconds(1.5)
+                .splineToLinearHeading(new Pose2d(-13.17, 8.59, Math.toRadians(90)), Math.toRadians(270))
+                .splineToLinearHeading(new Pose2d(-28.42, -6.61, Math.toRadians(0)), Math.toRadians(180))//2+0
+                .waitSeconds(1.5)
+                .strafeToLinearHeading(new Vector2d(-8, 30), Math.toRadians(180)) //pick up 2
+                .waitSeconds(1.5)
+                .strafeToLinearHeading(new Vector2d(-28.42, -6.61), Math.toRadians(0)) //3+0
+                .waitSeconds(1.5)
+                .strafeToLinearHeading(new Vector2d(-8, 30), Math.toRadians(180)) //pick up 3
+                .waitSeconds(1.5)
+                .strafeToLinearHeading(new Vector2d(-28.42, -6.61), Math.toRadians(0)) //4+0
+                .waitSeconds(1.5)
+                .strafeToLinearHeading(new Vector2d(-6, 32), Math.toRadians(180)); //park?
+
+        Action pathh = path.build();
+        waitForStart();
+
+        Actions.runBlocking(pathh);
+
+
+    }
+
+}
