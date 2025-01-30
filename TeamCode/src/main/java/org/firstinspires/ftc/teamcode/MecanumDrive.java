87b49716ac24a56ab523bc0b60a8a4585ea92af4
Malek Swilam <Malekgswilam@gmail.com>
2025-01-30 16:54:32 -0500

    changed speed slightly, and added auto file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -73,9 +73,9 @@ public class MecanumDrive {
         public double kA = 1;
 
         // path profile parameters (in inches)
-        public double maxWheelVel = 50;
-        public double minProfileAccel = -30;
-        public double maxProfileAccel = 50;
+        public double maxWheelVel = 120;
+        public double minProfileAccel = -60;
+        public double maxProfileAccel = 80;
 
         // turn profile parameters (in radians)
         public double maxAngVel = Math.PI; // shared with path
