9cf50e468c7b8efa86f0d59420a94d0deaed32f8
Vlad <vladimir6600@gmail.com>
2025-03-02 22:29:02 -0500

    Just changing motor names

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -225,10 +225,11 @@ public final class MecanumDrive {
 
         // TODO: make sure your config has motors with these names (or change them)
         //   see https://ftc-docs.firstinspires.org/en/latest/hardware_and_software_configuration/configuring/index.html
-        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
-        leftBack = hardwareMap.get(DcMotorEx.class, "leftBack");
-        rightBack = hardwareMap.get(DcMotorEx.class, "rightBack");
-        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        leftFront = hardwareMap.get(DcMotorEx.class, "FrontLeftMotor");
+        leftBack = hardwareMap.get(DcMotorEx.class, "BackLeftMotor");
+        rightBack = hardwareMap.get(DcMotorEx.class, "BackRightMotor");
+        rightFront = hardwareMap.get(DcMotorEx.class, "FrontRightMotor");
+
 
         leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
